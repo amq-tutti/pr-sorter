b3e9dde234b39727458c626271344c8f7d4b58ce
amq-tutti <tutti-amq@proton.me>
2026-05-18 21:38:53 +0300

    Update SI-VIS sorter description

--- customize/config.ts
+++ customize/config.ts
@@ -3,7 +3,7 @@ import type { AppConfig } from "../src/app/types";
 export const config = {
   localStoragePrefix: "si-vis",
   title: "SI-VIS",
-  description: "Party rank sorter SI-VIS songs",
+  description: "Party rank sorter for SI-VIS anime songs",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
