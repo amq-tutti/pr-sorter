f0ba792376ef38a922e6f9fcd77f8cff336510bc
amq-tutti <tutti-amq@proton.me>
2026-05-18 21:49:35 +0300

    Create Movie Anisongs sorter

--- customize/config.ts
+++ customize/config.ts
@@ -1,13 +1,12 @@
 import type { AppConfig } from "../src/app/types";
 
 export const config = {
-  localStoragePrefix: "princession-orchestra",
-  title: "Princession Orchestra",
-  description: "Party rank sorter for all Princession Orchestra songs (anime + image songs)",
+  localStoragePrefix: "movie-anisongs",
+  title: "Movie Anisongs",
+  description: "Party rank sorter for nominated songs from anime movies",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
     rankColumnHeader: "Rank",
-    scoreColumnHeader: "Score (optional)",
   },
 } satisfies AppConfig;
