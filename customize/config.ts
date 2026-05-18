d9ca5ff4fe96db57af5d5ea1269a318c4dc4a11b
amq-tutti <tutti-amq@proton.me>
2026-05-18 21:39:52 +0300

    Update Princession Orchestra sorter description

--- customize/config.ts
+++ customize/config.ts
@@ -3,7 +3,7 @@ import type { AppConfig } from "../src/app/types";
 export const config = {
   localStoragePrefix: "princession-orchestra",
   title: "Princession Orchestra",
-  description: "Party rank sorter for all of the songs in Princession Orchestra",
+  description: "Party rank sorter for all Princession Orchestra songs (anime + image songs)",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
