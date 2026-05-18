495e4238b1a60bee659f7f26b17b4f63781c1165
amq-tutti <tutti-amq@proton.me>
2026-05-18 22:03:29 +0300

    Add Google Sheet integration for 2015 sorter

--- customize/config.ts
+++ customize/config.ts
@@ -4,14 +4,9 @@ export const config = {
   localStoragePrefix: "2015-nominations",
   title: "2015 Nominations",
   description: "Party rank sorter for nominated anime songs from 2015",
-  // Uncomment the following lines to enable Google Sheets integration. Make sure to fill in the correct values.
-  // googleSheets: {
-  //   clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
-  //   appId: "601853881036",
-  //   rankColumnHeader: "Rank",
-  //   This setting will only work if `scoreColumnHeader` is not set, allowing users to specify their own score column header in the sheet.
-  //   If `scoreColumnHeader` is set, that value will be used as the score column header and users won't be able to change it.
-  //   allowCustomScoreColumn: true,
-  //   scoreColumnHeader: "Score (optional)",
-  // },
+  googleSheets: {
+    clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
+    appId: "601853881036",
+    rankColumnHeader: "Rank",
+  },
 } satisfies AppConfig;
