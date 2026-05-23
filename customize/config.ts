f2b3371eb106365c955945d9078cc900b3070f51
amq-tutti <tutti-amq@proton.me>
2026-05-23 16:45:10 +0300

    Remove trailing period in description

--- customize/config.ts
+++ customize/config.ts
@@ -3,7 +3,7 @@ import type { AppConfig } from "../src/app/types";
 export const config = {
   localStoragePrefix: "lozareena",
   title: "Lozareena",
-  description: "Party rank sorter for Lozareena anime songs.",
+  description: "Party rank sorter for Lozareena anime songs",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
