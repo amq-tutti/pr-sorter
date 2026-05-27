1ed899edc4f6efeaf4a612285f9db377532a1bcf
amq-tutti <tutti-amq@proton.me>
2026-05-27 18:42:46 +0300

    Allow adding score column for sorter

--- customize/config.ts
+++ customize/config.ts
@@ -8,5 +8,6 @@ export const config = {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
     rankColumnHeader: "Rank",
+    allowCustomScoreColumn: true,
   },
 } satisfies AppConfig;
