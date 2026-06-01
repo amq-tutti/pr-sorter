56f4e70c0b763ff5102570fb61db57ddec668f2d
amq-tutti <tutti-amq@proton.me>
2026-06-01 23:04:27 +0300

    Add category

--- customize/config.ts
+++ customize/config.ts
@@ -4,6 +4,7 @@ export const config = {
   localStoragePrefix: "2015-nominations",
   title: "2015 Nominations",
   description: "Party rank sorter for nominated anime songs from 2015",
+  category: "Nominations",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
