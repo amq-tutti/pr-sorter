d8bfd9facc1a3c5e8b1e8cbd17f71deabf072d60
amq-tutti <tutti-amq@proton.me>
2026-06-01 23:07:59 +0300

    Add category

--- customize/config.ts
+++ customize/config.ts
@@ -4,6 +4,7 @@ export const config = {
   localStoragePrefix: "lozareena",
   title: "Lozareena",
   description: "Party rank sorter for Lozareena anime songs",
+  category: "Artist",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
