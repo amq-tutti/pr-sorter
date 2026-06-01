cb352e3761f910b2920d1d19e1c11a00e61111e9
amq-tutti <tutti-amq@proton.me>
2026-06-01 23:09:02 +0300

    Add category

--- customize/config.ts
+++ customize/config.ts
@@ -4,6 +4,7 @@ export const config = {
   localStoragePrefix: "2026-spring-eds",
   title: "2026 Spring EDs",
   description: "Party rank sorter for all 2026 Spring anime endings",
+  category: "Seasonal",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
