e47251d3ffc405d226af0f2f736e6d45ba770b32
amq-tutti <tutti-amq@proton.me>
2026-06-01 23:07:36 +0300

    Add category

--- customize/config.ts
+++ customize/config.ts
@@ -4,6 +4,7 @@ export const config = {
   localStoragePrefix: "maaya-sakamoto",
   title: "Maaya Sakamoto",
   description: "Party rank sorter for Maaya Sakamoto anime songs",
+  category: "Artist",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
