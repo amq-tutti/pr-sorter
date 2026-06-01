f0f2a67d7ee244ea07ead3961e26eab522fdcc09
amq-tutti <tutti-amq@proton.me>
2026-06-01 14:16:01 +0300

    Fix song IDs

--- customize/songList.ts
+++ customize/songList.ts
@@ -3,76 +3,76 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-   {
+  {
     "id": 1,
     "anime": "Code Geass: Hangyaku no Lelouch R2 - Opening 1",
     "name": "\"Face2\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/1tc7hx.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/tgwwtb.mp3",
+    "video": "https://naedist.animemusicquiz.com/1tc7hx.webm",
+    "mp3": "https://naedist.animemusicquiz.com/tgwwtb.mp3",
     "full": "https://www.youtube.com/watch?v=PDiGcWh5lOE"
   },
   {
     "id": 2,
-    "anime": "Edens Zero - Ending 2",
-    "name": "\"my star\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/nwf740.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/83hdfr.mp3",
-    "full": "https://www.youtube.com/watch?v=uZ8MFoT8ROs"
-  },
-  {
-    "id": 3,
-    "anime": "Eiga Entotsu Machi no Poupelle - Ending 1",
-    "name": "\"Entotsu Machi no Poupelle\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/3aqxzf.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/6kb22s.mp3",
-    "full": "https://www.youtube.com/watch?v=KYSrLAKicE0"
-  },
-  {
-    "id": 4,
-    "anime": "Eiga Odekake Kozame: Tokai no Otomodachi - Ending 1",
-    "name": "\"where you come from\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/8wzi90.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/24iqvv.mp3",
-    "full": "https://www.youtube.com/watch?v=OpuYN_ESEBE"
-  },
-  {
-    "id": 5,
-    "anime": "Kabukichou Sherlock - Ending 1",
-    "name": "\"Hyakuoku Kounen\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/cyzrv0.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/7l9ylk.mp3",
-    "full": "https://www.youtube.com/watch?v=X26Jta6yg9k"
-  },
-  {
-    "id": 6,
-    "anime": "Karakuri Circus - Opening 3",
-    "name": "\"Over me\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/p1b3tl.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/mpzg95.mp3",
-    "full": "https://www.youtube.com/watch?v=HgTjmHIAID4"
-  },
-  {
-    "id": 7,
-    "anime": "Karakuri Circus - Ending 1",
-    "name": "\"Marionette\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/xxvcwo.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/sukcwz.mp3",
-    "full": "https://www.youtube.com/watch?v=FPct5qq2Gus"
-  },
-  {
-    "id": 8,
     "anime": "Youkai Apartment no Yuuga na Nichijou - Opening 1",
     "name": "\"Good Night Mare\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/n1f8il.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/llmr3g.mp3",
+    "video": "https://naedist.animemusicquiz.com/n1f8il.webm",
+    "mp3": "https://naedist.animemusicquiz.com/llmr3g.mp3",
     "full": "https://www.youtube.com/watch?v=qQbVHBdth6s"
   },
   {
-    "id": 9,
+    "id": 3,
     "anime": "Youkai Apartment no Yuuga na Nichijou - Ending 2",
     "name": "\"Neiro\" by Lozareena",
-    "video": "https://nawdist.animemusicquiz.com/jp0gsp.webm",
-    "mp3": "https://nawdist.animemusicquiz.com/maotmk.mp3",
+    "video": "https://naedist.animemusicquiz.com/jp0gsp.webm",
+    "mp3": "https://naedist.animemusicquiz.com/maotmk.mp3",
     "full": "https://www.youtube.com/watch?v=pueK4wHI7TY"
+  },
+  {
+    "id": 4,
+    "anime": "Karakuri Circus - Opening 3",
+    "name": "\"Over me\" by Lozareena",
+    "video": "https://naedist.animemusicquiz.com/p1b3tl.webm",
+    "mp3": "https://naedist.animemusicquiz.com/mpzg95.mp3",
+    "full": "https://www.youtube.com/watch?v=HgTjmHIAID4"
+  },
+  {
+    "id": 5,
+    "anime": "Karakuri Circus - Ending 1",
+    "name": "\"Marionette\" by Lozareena",
+    "video": "https://naedist.animemusicquiz.com/xxvcwo.webm",
+    "mp3": "https://naedist.animemusicquiz.com/sukcwz.mp3",
+    "full": "https://www.youtube.com/watch?v=FPct5qq2Gus"
+  },
+  {
+    "id": 6,
+    "anime": "Kabukichou Sherlock - Ending 1",
+    "name": "\"Hyakuoku Kounen\" by Lozareena",
+    "video": "https://naedist.animemusicquiz.com/cyzrv0.webm",
+    "mp3": "https://naedist.animemusicquiz.com/7l9ylk.mp3",
+    "full": "https://www.youtube.com/watch?v=X26Jta6yg9k"
+  },
+  {
+    "id": 7,
+    "anime": "Eiga Entotsu Machi no Poupelle - Ending 1",
+    "name": "\"Entotsu Machi no Poupelle\" by Lozareena",
+    "video": "https://naedist.animemusicquiz.com/3aqxzf.webm",
+    "mp3": "https://naedist.animemusicquiz.com/6kb22s.mp3",
+    "full": "https://www.youtube.com/watch?v=KYSrLAKicE0"
+  },
+  {
+    "id": 8,
+    "anime": "Edens Zero - Ending 2",
+    "name": "\"my star\" by Lozareena",
+    "video": "https://naedist.animemusicquiz.com/nwf740.webm",
+    "mp3": "https://naedist.animemusicquiz.com/83hdfr.mp3",
+    "full": "https://www.youtube.com/watch?v=uZ8MFoT8ROs"
+  },
+  {
+    "id": 9,
+    "anime": "Eiga Odekake Kozame: Tokai no Otomodachi - Ending 1",
+    "name": "\"where you come from\" by Lozareena",
+    "video": "https://naedist.animemusicquiz.com/8wzi90.webm",
+    "mp3": "https://naedist.animemusicquiz.com/24iqvv.mp3",
+    "full": "https://www.youtube.com/watch?v=OpuYN_ESEBE"
   },
 ] satisfies Song[];
