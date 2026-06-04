af3ad45987f006d819df34578b7625556c26fff6
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:53:20 +0300

    Add voting deadline

--- customize/config.ts
+++ customize/config.ts
@@ -5,6 +5,7 @@ export const config = {
   title: "Maaya Sakamoto",
   description: "Party rank sorter for Maaya Sakamoto anime songs",
   tags: ["Artist"],
+  deadline: "2026-05-24T04:00:00.000Z",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
