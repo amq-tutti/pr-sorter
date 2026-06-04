807f32f6cd7f26bc96560b800441871280c3b8eb
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:53:19 +0300

    Add voting deadline

--- customize/config.ts
+++ customize/config.ts
@@ -5,6 +5,7 @@ export const config = {
   title: "2015 Nominations",
   description: "Party rank sorter for nominated anime songs from 2015",
   tags: ["Nominations"],
+  deadline: "2026-06-13T22:00:00.000Z",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
