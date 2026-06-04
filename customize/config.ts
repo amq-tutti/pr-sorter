a5bf1eb104fdfd9218270f3059565624ce3ad70e
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:53:19 +0300

    Add voting deadline

--- customize/config.ts
+++ customize/config.ts
@@ -5,6 +5,7 @@ export const config = {
   title: "2026 Spring EDs",
   description: "Party rank sorter for all 2026 Spring anime endings",
   tags: ["Seasonal"],
+  deadline: "2026-06-19T22:00:00.000Z",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
