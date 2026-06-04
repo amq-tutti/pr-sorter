9e6a907015e7bfdd2cd6043ec6dded1e3229bfb0
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:37:39 +0300

    Use tags instead of category

--- customize/config.ts
+++ customize/config.ts
@@ -4,7 +4,7 @@ export const config = {
   localStoragePrefix: "si-vis",
   title: "SI-VIS",
   description: "Party rank sorter for SI-VIS anime songs",
-  category: "Franchise",
+  tags: ["Franchise"],
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
