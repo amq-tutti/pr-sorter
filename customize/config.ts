ed191e67e0b2084fd5f25f3b0b44db3df4388bd8
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:53:20 +0300

    Add voting deadline

--- customize/config.ts
+++ customize/config.ts
@@ -4,6 +4,7 @@ export const config = {
   localStoragePrefix: "yorushika",
   title: "Yorushika",
   tags: ["Artist"],
+  deadline: "2026-06-09T20:00:00.000Z",
   description: "Party rank sorter for Yorushika anime songs",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
