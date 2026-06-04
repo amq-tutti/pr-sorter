e50ae7f2e1a021302891238a90e0308caa0f1cd7
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:37:38 +0300

    Use tags instead of category

--- customize/config.ts
+++ customize/config.ts
@@ -4,7 +4,7 @@ export const config = {
   localStoragePrefix: "maaya-sakamoto",
   title: "Maaya Sakamoto",
   description: "Party rank sorter for Maaya Sakamoto anime songs",
-  category: "Artist",
+  tags: ["Artist"],
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
