23b78610f114775f6335dc985c50370e2fff36ef
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:37:38 +0300

    Use tags instead of category

--- customize/config.ts
+++ customize/config.ts
@@ -3,7 +3,7 @@ import type { AppConfig } from "../src/app/types";
 export const config = {
   localStoragePrefix: "crystal-kay",
   title: "Crystal Kay",
-  category: "Artist",
+  tags: ["Artist"],
   description: "Party rank sorter for Crystal Kay anime song",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
