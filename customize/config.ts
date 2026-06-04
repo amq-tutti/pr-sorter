ea5047b85bcc0447d3b0b5ba18d09c41e97ebe94
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:37:37 +0300

    Use tags instead of category

--- customize/config.ts
+++ customize/config.ts
@@ -4,7 +4,7 @@ export const config = {
   localStoragePrefix: "2026-spring-ops",
   title: "2026 Spring OPs",
   description: "Party rank sorter for all 2026 Spring anime openings",
-  category: "Seasonal",
+  tags: ["Seasonal"],
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
