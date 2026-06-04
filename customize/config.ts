bcf272416eb3ac77a2166fa9184f748c2dac81e8
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:36:49 +0300

    Use tags instead of category

--- customize/config.ts
+++ customize/config.ts
@@ -3,7 +3,7 @@ import type { AppConfig } from "../src/app/types";
 export const config = {
   localStoragePrefix: "yorushika",
   title: "Yorushika",
-  category: "Artist",
+  tags: ["Artist"],
   description: "Party rank sorter for Yorushika anime songs",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
