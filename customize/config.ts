59a4232a5631b147cb59c26ec2578b5b06bf2c60
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:37:38 +0300

    Use tags instead of category

--- customize/config.ts
+++ customize/config.ts
@@ -4,7 +4,7 @@ export const config = {
   localStoragePrefix: "princession-orchestra",
   title: "Princession Orchestra",
   description: "Party rank sorter for all Princession Orchestra songs (anime + image songs)",
-  category: "Franchise",
+  tags: ["Franchise"],
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
