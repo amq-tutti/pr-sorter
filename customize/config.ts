7fb478935db356835112e684466c1af25ac75168
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:37:38 +0300

    Use tags instead of category

--- customize/config.ts
+++ customize/config.ts
@@ -4,7 +4,7 @@ export const config = {
   localStoragePrefix: "movie-anisongs",
   title: "Movie Anisongs",
   description: "Party rank sorter for nominated songs from anime movies",
-  category: "Nominations",
+  tags: ["Nominations"],
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
