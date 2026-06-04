33d20e111dbc7fb01360656279bedbe8e875a703
amq-tutti <tutti-amq@proton.me>
2026-06-05 00:37:38 +0300

    Use tags instead of category

--- customize/config.ts
+++ customize/config.ts
@@ -4,7 +4,7 @@ export const config = {
   localStoragePrefix: "lozareena",
   title: "Lozareena",
   description: "Party rank sorter for Lozareena anime songs",
-  category: "Artist",
+  tags: ["Artist"],
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
