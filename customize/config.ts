ecceeed9db42706d17b3ffd3cc30e47312494b71
amq-tutti <tutti-amq@proton.me>
2026-06-18 23:59:28 +0300

    Update deadline to new Date(...) for AppConfig compatibility

--- customize/config.ts
+++ customize/config.ts
@@ -5,7 +5,7 @@ export const config = {
   title: "Lozareena",
   description: "Party rank sorter for Lozareena anime songs",
   tags: ["Artist"],
-  deadline: "2026-06-01T20:00:00.000Z",
+  deadline: new Date("2026-06-01T20:00:00.000Z"),
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
