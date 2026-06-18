a3dfd4c3fd2437d529bef1dbf9e5f57946ca44a4
amq-tutti <tutti-amq@proton.me>
2026-06-18 23:59:26 +0300

    Update deadline to new Date(...) for AppConfig compatibility

--- customize/config.ts
+++ customize/config.ts
@@ -4,7 +4,7 @@ export const config = {
   localStoragePrefix: "crystal-kay",
   title: "Crystal Kay",
   tags: ["Artist"],
-  deadline: "2026-06-09T20:00:00.000Z",
+  deadline: new Date("2026-06-09T20:00:00.000Z"),
   description: "Party rank sorter for Crystal Kay anime song",
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
