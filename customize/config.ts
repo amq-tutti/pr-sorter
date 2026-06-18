a6471aa118402eed41ea1fb117c404af9e0f1123
amq-tutti <tutti-amq@proton.me>
2026-06-18 23:59:32 +0300

    Update deadline to new Date(...) for AppConfig compatibility

--- customize/config.ts
+++ customize/config.ts
@@ -5,7 +5,7 @@ export const config = {
   title: "Princession Orchestra",
   description: "Party rank sorter for all Princession Orchestra songs (anime + image songs)",
   tags: ["Franchise"],
-  deadline: "2026-06-11T22:00:00.000Z",
+  deadline: new Date("2026-06-11T22:00:00.000Z"),
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
