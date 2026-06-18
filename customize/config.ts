8a4b800671ebf9d7157e0879245338aae7880f11
amq-tutti <tutti-amq@proton.me>
2026-06-18 23:59:31 +0300

    Update deadline to new Date(...) for AppConfig compatibility

--- customize/config.ts
+++ customize/config.ts
@@ -5,7 +5,7 @@ export const config = {
   title: "Movie Anisongs",
   description: "Party rank sorter for nominated songs from anime movies",
   tags: ["Nominations"],
-  deadline: "2026-05-30T22:00:00.000Z",
+  deadline: new Date("2026-05-30T22:00:00.000Z"),
   googleSheets: {
     clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
     appId: "601853881036",
