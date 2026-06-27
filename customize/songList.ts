1893c27aa5ebc2ff149e5e29ff2991ccf7a77b7a
amq-tutti <tutti-amq@proton.me>
2026-06-27 15:26:31 +0300

    Add Suara sorter

--- customize/songList.ts
+++ customize/songList.ts
@@ -3,14 +3,228 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {"id": 1, "anime": "0-saiji Start Dash Monogatari Season 2 - Ending 1", "name": "\"Beyond the Trajectory\" by Chata", "video": "https://eudist.animemusicquiz.com/5u2mhk.webm", "mp3": null},
-    {"id": 2, "anime": "1-punkan dake Furete mo Ii yo... Share House no Himitsu Rule. - Ending 1", "name": "\"Session Sadame Night\" by Dai Takanashi", "video": "https://eudist.animemusicquiz.com/f23gq2.webm", "mp3": null},
-    {"id": 3, "anime": "A-Rank Party wo Ridatsu shita Ore wa, Moto Oshiego-tachi to Meikyuu Shinbu wo Mezasu. - Ending 1", "name": "\"Treasure Chest\" by Yuki Tanaka", "video": "https://eudist.animemusicquiz.com/agsvoz.webm", "mp3": "https://eudist.animemusicquiz.com/i3vp9q.mp3"},
-    {"id": 4, "anime": "Akuyaku Reijou Tensei Ojisan - Ending 1", "name": "\"Matsuken Samba II\" by Kazuhiko Inoue, M・A・O", "video": "https://eudist.animemusicquiz.com/e8q47f.webm", "mp3": "https://eudist.animemusicquiz.com/dje0hr.mp3"},
-    {"id": 5, "anime": "Amagami-san Chi no Enmusubi - Ending 2", "name": "\"Kamisama no Iu Toori!\" by ≠ME", "video": "https://eudist.animemusicquiz.com/p4etz1.webm", "mp3": "https://eudist.animemusicquiz.com/wz8z3p.mp3"},
-    {"id": 6, "anime": "Ameku Takao no Suiri Karte - Ending 1", "name": "\"will be fine\" by Gospellers feat. Anly", "video": "https://eudist.animemusicquiz.com/0spf2m.webm", "mp3": "https://eudist.animemusicquiz.com/iagxs7.mp3"},
-    {"id": 7, "anime": "Ao no Exorcist: Yosuga-hen - Ending 1", "name": "\"Overlap\" by Shiyui", "video": "https://eudist.animemusicquiz.com/3z1d8i.webm", "mp3": "https://eudist.animemusicquiz.com/162vdv.mp3"},
-    {"id": 8, "anime": "Ao no Hako - Ending 2", "name": "\"Contrast\" by TOMOO", "video": "https://eudist.animemusicquiz.com/qmcqk3.webm", "mp3": "https://eudist.animemusicquiz.com/l2sgqo.mp3"},
-    {"id": 9, "anime": "Ao no Miburo - Ending 2", "name": "\"Fragment\" by osage", "video": "https://eudist.animemusicquiz.com/8b8iva.webm", "mp3": "https://eudist.animemusicquiz.com/0yt4lm.mp3"},
-    {"id": 10, "anime": "Arafou Otoko no Isekai Tsuuhan - Ending 1", "name": "\"Aicraft\" by Kiminone", "video": "https://eudist.animemusicquiz.com/ohlrpc.webm", "mp3": "https://eudist.animemusicquiz.com/74jbjn.mp3"},
+    {
+        "id": 1,
+        "anime": "Utawarerumono - Opening 1",
+        "name": "\"Musouka\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/t5bzh7.webm",
+        "mp3": "https://naedist.animemusicquiz.com/kksj9x.mp3"
+    },
+    {
+        "id": 2,
+        "anime": "Utawarerumono - Ending 2",
+        "name": "\"Kimi ga Tame\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/34njnq.webm",
+        "mp3": "https://naedist.animemusicquiz.com/au342f.mp3"
+    },
+    {
+        "id": 3,
+        "anime": "To Heart 2 - Ending 1",
+        "name": "\"Tomoshibi\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/op30qu.webm",
+        "mp3": "https://naedist.animemusicquiz.com/c0j3hq.mp3"
+    },
+    {
+        "id": 4,
+        "anime": "Asatte no Houkou. - Opening 1",
+        "name": "\"Hikari no Kisetsu\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/anti78.webm",
+        "mp3": "https://naedist.animemusicquiz.com/iwa50r.mp3"
+    },
+    {
+        "id": 5,
+        "anime": "Asatte no Houkou. - Insert Song",
+        "name": "\"Kasa\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/om88x1.webm",
+        "mp3": "https://naedist.animemusicquiz.com/gr7m3l.mp3"
+    },
+    {
+        "id": 6,
+        "anime": "To Heart 2 OVA - Opening 1",
+        "name": "\"Ichibanboshi\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/un9669.webm",
+        "mp3": "https://naedist.animemusicquiz.com/4xi5v2.mp3"
+    },
+    {
+        "id": 7,
+        "anime": "To Heart 2 OVA - Ending 3",
+        "name": "\"I am\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/cl9vwn.webm",
+        "mp3": "https://naedist.animemusicquiz.com/z796bh.mp3"
+    },
+    {
+        "id": 8,
+        "anime": "KimiKiss Pure Rouge - Ending 2",
+        "name": "\"Wasurenaide\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/5y6j57.webm",
+        "mp3": "https://naedist.animemusicquiz.com/vhly7y.mp3"
+    },
+    {
+        "id": 9,
+        "anime": "Blue Drop: Tenshi-tachi no Gikyoku - Opening 1",
+        "name": "\"BLUE\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/y5qgj6.webm",
+        "mp3": "https://naedist.animemusicquiz.com/px0ibz.mp3"
+    },
+    {
+        "id": 10,
+        "anime": "Blue Drop: Tenshi-tachi no Gikyoku - Ending 1",
+        "name": "\"Tsubomi -blue dreams-\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/67s9d7.webm",
+        "mp3": "https://naedist.animemusicquiz.com/0pceg9.mp3"
+    },
+    {
+        "id": 11,
+        "anime": "Utawarerumono OVA - Opening 1",
+        "name": "\"adamant faith\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/028uaf.webm",
+        "mp3": "https://naedist.animemusicquiz.com/zrpwrf.mp3"
+    },
+    {
+        "id": 12,
+        "anime": "Tears to Tiara - Opening 1",
+        "name": "\"Free and Dream\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/e6m1nj.webm",
+        "mp3": "https://naedist.animemusicquiz.com/aq2ve7.mp3"
+    },
+    {
+        "id": 13,
+        "anime": "Tears to Tiara - Ending 3",
+        "name": "\"memory\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/i54k1f.webm",
+        "mp3": "https://naedist.animemusicquiz.com/ryir0v.mp3"
+    },
+    {
+        "id": 14,
+        "anime": "White Album - Ending 1",
+        "name": "\"Maiochiru Yuki no You ni\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/l2vxaa.webm",
+        "mp3": "https://naedist.animemusicquiz.com/wd845j.mp3"
+    },
+    {
+        "id": 15,
+        "anime": "White Album - Ending 2",
+        "name": "\"Akai Ito\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/vnkkzy.webm",
+        "mp3": "https://naedist.animemusicquiz.com/eb5hp3.mp3"
+    },
+    {
+        "id": 16,
+        "anime": "To Heart 2 Dungeon Travelers - Opening 1",
+        "name": "\"Niji-iro no Mirai\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/ll4gsz.webm",
+        "mp3": "https://naedist.animemusicquiz.com/wat5gn.mp3"
+    },
+    {
+        "id": 17,
+        "anime": "Cardfight!! Vanguard: Link Joker-hen - Ending 4",
+        "name": "\"Fly away -Oozora e-\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/xvk5ci.webm",
+        "mp3": "https://naedist.animemusicquiz.com/sv1qy1.mp3"
+    },
+    {
+        "id": 18,
+        "anime": "Future Card Buddyfight - Opening 1",
+        "name": "\"Card of the Future\" by Psychic Lover×Suara",
+        "video": "https://naedist.animemusicquiz.com/qk68o6.webm",
+        "mp3": "https://naedist.animemusicquiz.com/r00ur5.mp3"
+    },
+    {
+        "id": 19,
+        "anime": "Cardfight!! Vanguard: Legion Mate-hen - Opening 1",
+        "name": "\"V-ROAD\" by BUSHI★7",
+        "video": "https://naedist.animemusicquiz.com/k0wy80.webm",
+        "mp3": "https://naedist.animemusicquiz.com/jgszjl.mp3"
+    },
+    {
+        "id": 20,
+        "anime": "Gekijouban Cardfight!! Vanguard: Neon Messiah - Opening 1",
+        "name": "\"V-ROAD (symphony ver.)\" by BUSHI★7",
+        "video": "https://naedist.animemusicquiz.com/rwjgvl.webm",
+        "mp3": "https://naedist.animemusicquiz.com/tsqc2l.mp3"
+    },
+    {
+        "id": 21,
+        "anime": "Utawarerumono: Itsuwari no Kamen - Opening 1",
+        "name": "\"Fuantei na Kamisama\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/pbzsm6.webm",
+        "mp3": "https://naedist.animemusicquiz.com/5kne9a.mp3"
+    },
+    {
+        "id": 22,
+        "anime": "Utawarerumono: Itsuwari no Kamen - Opening 2",
+        "name": "\"Amakakeru Hoshi\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/yg4voo.webm",
+        "mp3": "https://naedist.animemusicquiz.com/347uaj.mp3"
+    },
+    {
+        "id": 23,
+        "anime": "Utawarerumono: Itsuwari no Kamen - Ending 1",
+        "name": "\"Yume ka Utsutsu ka\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/46huhp.webm",
+        "mp3": "https://naedist.animemusicquiz.com/x1r5nb.mp3"
+    },
+    {
+        "id": 24,
+        "anime": "Utawarerumono: Itsuwari no Kamen - Ending 2",
+        "name": "\"Hoshi Furu Sora Aogimite\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/o4nx06.webm",
+        "mp3": "https://naedist.animemusicquiz.com/34gtgq.mp3"
+    },
+    {
+        "id": 25,
+        "anime": "Utawarerumono: Itsuwari no Kamen - Insert Song",
+        "name": "\"Nuedori\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/k5qy4k.webm",
+        "mp3": "https://naedist.animemusicquiz.com/38cen2.mp3"
+    },
+    {
+        "id": 26,
+        "anime": "Nil Admirari no Tenbin - Insert Song",
+        "name": "\"Homura no Tori\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/mbg39i.webm",
+        "mp3": "https://naedist.animemusicquiz.com/nuco8i.mp3"
+    },
+    {
+        "id": 27,
+        "anime": "Utawarerumono: Futari no Hakuoro - Opening 1",
+        "name": "\"Hito Nanda\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/13nqxw.webm",
+        "mp3": "https://naedist.animemusicquiz.com/zwv5k4.mp3"
+    },
+    {
+        "id": 28,
+        "anime": "Utawarerumono: Futari no Hakuoro - Ending 2",
+        "name": "\"Hyakunichisou\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/1mjtmp.webm",
+        "mp3": "https://naedist.animemusicquiz.com/ts9mui.mp3"
+    },
+    {
+        "id": 29,
+        "anime": "Utawarerumono: Futari no Hakuoro - Ending 3",
+        "name": "\"Musouka 2016\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/b07ei8.webm",
+        "mp3": "https://naedist.animemusicquiz.com/yjs1u8.mp3"
+    },
+    {
+        "id": 30,
+        "anime": "Utawarerumono: Futari no Hakuoro - Insert Song",
+        "name": "\"Koiyume\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/dv6vr3.webm",
+        "mp3": "https://naedist.animemusicquiz.com/mwnhmx.mp3"
+    },
+    {
+        "id": 31,
+        "anime": "Utawarerumono: Futari no Hakuoro - Insert Song",
+        "name": "\"Hikari\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/gc29hq.webm",
+        "mp3": "https://naedist.animemusicquiz.com/8rh10g.mp3"
+    },
+    {
+        "id": 32,
+        "anime": "Utawarerumono: Futari no Hakuoro - Insert Song",
+        "name": "\"Kimi ga Tame 2016\" by Suara",
+        "video": "https://naedist.animemusicquiz.com/nllynv.webm",
+        "mp3": "https://naedist.animemusicquiz.com/jxm0ux.mp3"
+    },
 ] satisfies Song[];
