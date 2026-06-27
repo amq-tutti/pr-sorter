8b5d3bcdd2602b13965a6c8ae4bd3aeab60986c5
amq-tutti <tutti-amq@proton.me>
2026-06-27 15:45:32 +0300

    Add missing Google Sheets config

--- customize/config.ts
+++ customize/config.ts
@@ -8,14 +8,10 @@ export const config = {
   tags: ["Artist"],
   // Optional voting deadline (ISO 8601). Written into the generated sorter-index.json.
   deadline: new Date("2026-07-08T04:00:00.000Z"),
-  // Uncomment the following lines to enable Google Sheets integration. Make sure to fill in the correct values.
-  // googleSheets: {
-  //   clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
-  //   appId: "601853881036",
-  //   rankColumnHeader: "Rank",
-  //   This setting will only work if `scoreColumnHeader` is not set, allowing users to specify their own score column header in the sheet.
-  //   If `scoreColumnHeader` is set, that value will be used as the score column header and users won't be able to change it.
-  //   allowCustomScoreColumn: true,
-  //   scoreColumnHeader: "Score (optional)",
-  // },
+  googleSheets: {
+    clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
+    appId: "601853881036",
+    rankColumnHeader: "Rank",
+    scoreColumnHeader: "Score",
+  },
 } satisfies AppConfig;
