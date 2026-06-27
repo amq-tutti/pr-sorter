566c55c76ca3fe088fc86a5d8f7e5ba5fda70d65
amq-tutti <tutti-amq@proton.me>
2026-06-27 15:44:23 +0300

    Add Code Geass OP/ED sorter

--- customize/songList.ts
+++ customize/songList.ts
@@ -3,14 +3,207 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {"id": 1, "anime": "0-saiji Start Dash Monogatari Season 2 - Ending 1", "name": "\"Beyond the Trajectory\" by Chata", "video": "https://eudist.animemusicquiz.com/5u2mhk.webm", "mp3": null},
-    {"id": 2, "anime": "1-punkan dake Furete mo Ii yo... Share House no Himitsu Rule. - Ending 1", "name": "\"Session Sadame Night\" by Dai Takanashi", "video": "https://eudist.animemusicquiz.com/f23gq2.webm", "mp3": null},
-    {"id": 3, "anime": "A-Rank Party wo Ridatsu shita Ore wa, Moto Oshiego-tachi to Meikyuu Shinbu wo Mezasu. - Ending 1", "name": "\"Treasure Chest\" by Yuki Tanaka", "video": "https://eudist.animemusicquiz.com/agsvoz.webm", "mp3": "https://eudist.animemusicquiz.com/i3vp9q.mp3"},
-    {"id": 4, "anime": "Akuyaku Reijou Tensei Ojisan - Ending 1", "name": "\"Matsuken Samba II\" by Kazuhiko Inoue, M・A・O", "video": "https://eudist.animemusicquiz.com/e8q47f.webm", "mp3": "https://eudist.animemusicquiz.com/dje0hr.mp3"},
-    {"id": 5, "anime": "Amagami-san Chi no Enmusubi - Ending 2", "name": "\"Kamisama no Iu Toori!\" by ≠ME", "video": "https://eudist.animemusicquiz.com/p4etz1.webm", "mp3": "https://eudist.animemusicquiz.com/wz8z3p.mp3"},
-    {"id": 6, "anime": "Ameku Takao no Suiri Karte - Ending 1", "name": "\"will be fine\" by Gospellers feat. Anly", "video": "https://eudist.animemusicquiz.com/0spf2m.webm", "mp3": "https://eudist.animemusicquiz.com/iagxs7.mp3"},
-    {"id": 7, "anime": "Ao no Exorcist: Yosuga-hen - Ending 1", "name": "\"Overlap\" by Shiyui", "video": "https://eudist.animemusicquiz.com/3z1d8i.webm", "mp3": "https://eudist.animemusicquiz.com/162vdv.mp3"},
-    {"id": 8, "anime": "Ao no Hako - Ending 2", "name": "\"Contrast\" by TOMOO", "video": "https://eudist.animemusicquiz.com/qmcqk3.webm", "mp3": "https://eudist.animemusicquiz.com/l2sgqo.mp3"},
-    {"id": 9, "anime": "Ao no Miburo - Ending 2", "name": "\"Fragment\" by osage", "video": "https://eudist.animemusicquiz.com/8b8iva.webm", "mp3": "https://eudist.animemusicquiz.com/0yt4lm.mp3"},
-    {"id": 10, "anime": "Arafou Otoko no Isekai Tsuuhan - Ending 1", "name": "\"Aicraft\" by Kiminone", "video": "https://eudist.animemusicquiz.com/ohlrpc.webm", "mp3": "https://eudist.animemusicquiz.com/74jbjn.mp3"},
+    {
+        "id": 1,
+        "anime": "Code Geass: Hangyaku no Lelouch - Opening 1",
+        "name": "\"COLORS\" by FLOW",
+        "video": "https://naedist.animemusicquiz.com/w9hgpy.webm",
+        "mp3": "https://naedist.animemusicquiz.com/ap6khk.mp3"
+    },
+    {
+        "id": 2,
+        "anime": "Code Geass: Hangyaku no Lelouch - Opening 2",
+        "name": "\"Kaidoku Funou\" by Jinn",
+        "video": "https://naedist.animemusicquiz.com/bzubbg.webm",
+        "mp3": "https://naedist.animemusicquiz.com/2jhoy4.mp3"
+    },
+    {
+        "id": 3,
+        "anime": "Code Geass: Hangyaku no Lelouch - Opening 3",
+        "name": "\"Hitomi no Tsubasa\" by access",
+        "video": "https://naedist.animemusicquiz.com/6rb9aq.webm",
+        "mp3": "https://naedist.animemusicquiz.com/h03p2z.mp3"
+    },
+    {
+        "id": 4,
+        "anime": "Code Geass: Hangyaku no Lelouch - Opening 1",
+        "name": "\"DICE\" by FLOW",
+        "video": "https://naedist.animemusicquiz.com/lzvdml.webm",
+        "mp3": "https://naedist.animemusicquiz.com/tbvmm0.mp3"
+    },
+    {
+        "id": 5,
+        "anime": "Code Geass: Hangyaku no Lelouch - Opening 2",
+        "name": "\"PHOENIX PRAYER\" by Eir Aoi",
+        "video": "https://naedist.animemusicquiz.com/ocg7or.webm",
+        "mp3": "https://naedist.animemusicquiz.com/lesxsw.mp3"
+    },
+    {
+        "id": 6,
+        "anime": "Code Geass: Hangyaku no Lelouch - Ending 1",
+        "name": "\"Yuukyou Seishunka\" by ALI PROJECT",
+        "video": "https://naedist.animemusicquiz.com/gatvaa.webm",
+        "mp3": "https://naedist.animemusicquiz.com/p68lp5.mp3"
+    },
+    {
+        "id": 7,
+        "anime": "Code Geass: Hangyaku no Lelouch - Ending 2",
+        "name": "\"Mosaic Kakera\" by SunSet Swish",
+        "video": "https://naedist.animemusicquiz.com/uw9ctc.webm",
+        "mp3": "https://naedist.animemusicquiz.com/m8l6kk.mp3"
+    },
+    {
+        "id": 8,
+        "anime": "Code Geass: Hangyaku no Lelouch - Ending 1",
+        "name": "\"will-ill\" by TK from Ling Tosite Sigure",
+        "video": "https://naedist.animemusicquiz.com/egj275.webm",
+        "mp3": "https://naedist.animemusicquiz.com/i8tsy2.mp3"
+    },
+    {
+        "id": 9,
+        "anime": "Code Geass: Hangyaku no Lelouch - Ending 2",
+        "name": "\"SAKURA BURST\" by Cö shu Nie",
+        "video": "https://naedist.animemusicquiz.com/raxu8w.webm",
+        "mp3": "https://naedist.animemusicquiz.com/lo4pah.mp3"
+    },
+    {
+        "id": 10,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Opening 1",
+        "name": "\"O2\" by ORANGE RANGE",
+        "video": "https://naedist.animemusicquiz.com/c6vhnz.webm",
+        "mp3": "https://naedist.animemusicquiz.com/fi44mn.mp3"
+    },
+    {
+        "id": 11,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Opening 2",
+        "name": "\"WORLD END\" by FLOW",
+        "video": "https://naedist.animemusicquiz.com/b9b56l.webm",
+        "mp3": "https://naedist.animemusicquiz.com/rsn4df.mp3"
+    },
+    {
+        "id": 12,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Opening 1",
+        "name": "\"Face2\" by Lozareena",
+        "video": "https://naedist.animemusicquiz.com/1tc7hx.webm",
+        "mp3": "https://naedist.animemusicquiz.com/tgwwtb.mp3"
+    },
+    {
+        "id": 13,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Opening 2",
+        "name": "\"Daydream Believer\" by FLOW×ORANGE RANGE",
+        "video": "https://naedist.animemusicquiz.com/ljc8kp.webm",
+        "mp3": "https://naedist.animemusicquiz.com/lgx8fb.mp3"
+    },
+    {
+        "id": 14,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Ending 1",
+        "name": "\"Shiawase Neiro\" by ORANGE RANGE",
+        "video": "https://naedist.animemusicquiz.com/4v5i7s.webm",
+        "mp3": "https://naedist.animemusicquiz.com/6k5b1h.mp3"
+    },
+    {
+        "id": 15,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Ending 2",
+        "name": "\"Waga Routashi Aku no Hana\" by ALI PROJECT",
+        "video": "https://naedist.animemusicquiz.com/rdrfkz.webm",
+        "mp3": "https://naedist.animemusicquiz.com/33lbv6.mp3"
+    },
+    {
+        "id": 16,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Ending 1",
+        "name": "\"Mushoku Toumei\" by Yutaro Yamashita",
+        "video": "https://naedist.animemusicquiz.com/stkgom.webm",
+        "mp3": "https://naedist.animemusicquiz.com/5c0yeo.mp3"
+    },
+    {
+        "id": 17,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Ending 2",
+        "name": "\"Z.E.R.O.\" by BLUE ENCOUNT",
+        "video": "https://naedist.animemusicquiz.com/dtloou.webm",
+        "mp3": "https://naedist.animemusicquiz.com/888r93.mp3"
+    },
+    {
+        "id": 18,
+        "anime": "Code Geass: Boukoku no Akito - Ending 1",
+        "name": "\"More Than Words\" by Maaya Sakamoto",
+        "video": "https://naedist.animemusicquiz.com/uezn4o.webm",
+        "mp3": "https://naedist.animemusicquiz.com/efq7p0.mp3"
+    },
+    {
+        "id": 19,
+        "anime": "Code Geass: Boukoku no Akito - Ending 2",
+        "name": "\"Arco\" by Maaya Sakamoto",
+        "video": "https://naedist.animemusicquiz.com/stp1bp.webm",
+        "mp3": "https://naedist.animemusicquiz.com/eyp3g3.mp3"
+    },
+    {
+        "id": 20,
+        "anime": "Code Geass: Hangyaku no Lelouch Special Edition 'Black Rebellion' - Opening 1",
+        "name": "\"Innocent Days\" by Hitomi",
+        "video": "https://naedist.animemusicquiz.com/f3isty.webm",
+        "mp3": "https://naedist.animemusicquiz.com/rcbmvc.mp3"
+    },
+    {
+        "id": 21,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 Special Edition 'Zero Requiem' - Opening 1",
+        "name": "\"Lullaby of M\" by Hitomi",
+        "video": "https://naedist.animemusicquiz.com/ztuqkt.webm",
+        "mp3": "https://naedist.animemusicquiz.com/0z8qyj.mp3"
+    },
+    {
+        "id": 22,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 Special Edition 'Zero Requiem' - Ending 1",
+        "name": "\"Continued Story\" by Hitomi",
+        "video": "https://naedist.animemusicquiz.com/8nnztk.webm",
+        "mp3": "https://naedist.animemusicquiz.com/ylkpyc.mp3"
+    },
+    {
+        "id": 23,
+        "anime": "Code Geass: Hangyaku no Lelouch Remake Movies - Ending 1",
+        "name": "\"Nunnally\" by Hitomi",
+        "video": "https://naedist.animemusicquiz.com/l0fund.webm",
+        "mp3": "https://naedist.animemusicquiz.com/h0kbyn.mp3"
+    },
+    {
+        "id": 24,
+        "anime": "Code Geass: Hangyaku no Lelouch Remake Movies - Ending 2",
+        "name": "\"The Moon\" by Sakura Fujiwara",
+        "video": "https://naedist.animemusicquiz.com/deyfo6.webm",
+        "mp3": "https://naedist.animemusicquiz.com/m5y243.mp3"
+    },
+    {
+        "id": 25,
+        "anime": "Code Geass: Hangyaku no Lelouch Remake Movies - Ending 3",
+        "name": "\"NE:ONE\" by Survive Said The Prophet",
+        "video": "https://naedist.animemusicquiz.com/lkjzs8.webm",
+        "mp3": "https://naedist.animemusicquiz.com/lzib4n.mp3"
+    },
+    {
+        "id": 26,
+        "anime": "Code Geass: Fukkatsu no Lelouch - Opening 1",
+        "name": "\"Kono Sekai de\" by Leo Ieiri",
+        "video": "https://naedist.animemusicquiz.com/dr2d8b.webm",
+        "mp3": "https://naedist.animemusicquiz.com/r4lxqu.mp3"
+    },
+    {
+        "id": 27,
+        "anime": "Code Geass: Fukkatsu no Lelouch - Ending 1",
+        "name": "\"Revive\" by UNIONE",
+        "video": "https://naedist.animemusicquiz.com/cjlh6g.webm",
+        "mp3": "https://naedist.animemusicquiz.com/vne9cv.mp3"
+    },
+    {
+        "id": 28,
+        "anime": "Code Geass: Dakkan no Rozé - Opening 1",
+        "name": "\"Running In My Head\" by MIYAVI",
+        "video": "https://naedist.animemusicquiz.com/osxxxu.webm",
+        "mp3": "https://naedist.animemusicquiz.com/glywpk.mp3"
+    },
+    {
+        "id": 29,
+        "anime": "Code Geass: Dakkan no Rozé - Ending 1",
+        "name": "\"Rozé (Prod.TeddyLoid)\" by Hikari Mitsushima",
+        "video": "https://naedist.animemusicquiz.com/1lpaik.webm",
+        "mp3": "https://naedist.animemusicquiz.com/pnaayj.mp3"
+    },
 ] satisfies Song[];
