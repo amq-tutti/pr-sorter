416695cec64b218a9914ab35f75c5bef269d6b61
amq-tutti <tutti-amq@proton.me>
2026-07-21 01:21:22 +0300

    Create "C" anisongs sorter

--- customize/config.ts
+++ customize/config.ts
@@ -1,21 +1,17 @@
 import type { AppConfig } from "../src/app/types";
 
 export const config = {
-  localStoragePrefix: "test-sorter",
-  title: "Test Sorter",
-  description: "Party rank sorter for your custom list of songs.",
+  localStoragePrefix: "c-anisongs",
+  title: '"C" Anisongs',
+  description: "Party rank sorter for anisongs starting with the letter C.",
   // Tags group this sorter under one or more headers on the collection homepage.
-  tags: ["Example"],
+  tags: ["Nominations"],
   // Optional voting deadline (ISO 8601). Written into the generated sorter-index.json.
-  deadline: new Date("2026-06-05T21:59:00.000Z"),
-  // Uncomment the following lines to enable Google Sheets integration. Make sure to fill in the correct values.
-  // googleSheets: {
-  //   clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
-  //   appId: "601853881036",
-  //   rankColumnHeader: "Rank",
-  //   This setting will only work if `scoreColumnHeader` is not set, allowing users to specify their own score column header in the sheet.
-  //   If `scoreColumnHeader` is set, that value will be used as the score column header and users won't be able to change it.
-  //   allowCustomScoreColumn: true,
-  //   scoreColumnHeader: "Score (optional)",
-  // },
+  deadline: new Date("2026-08-10T03:59:00.000Z"),
+  googleSheets: {
+    clientId: "601853881036-d54ok384qlquqv7h6arh4j5h4e2d1vm5.apps.googleusercontent.com",
+    appId: "601853881036",
+    rankColumnHeader: "Rank",
+    scoreColumnHeader: "Score",
+  },
 } satisfies AppConfig;
--- customize/songList.ts
+++ customize/songList.ts
@@ -3,14 +3,396 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {"id": 1, "anime": "0-saiji Start Dash Monogatari Season 2 - Ending 1", "name": "\"Beyond the Trajectory\" by Chata", "video": "https://eudist.animemusicquiz.com/5u2mhk.webm", "mp3": null},
-    {"id": 2, "anime": "1-punkan dake Furete mo Ii yo... Share House no Himitsu Rule. - Ending 1", "name": "\"Session Sadame Night\" by Dai Takanashi", "video": "https://eudist.animemusicquiz.com/f23gq2.webm", "mp3": null},
-    {"id": 3, "anime": "A-Rank Party wo Ridatsu shita Ore wa, Moto Oshiego-tachi to Meikyuu Shinbu wo Mezasu. - Ending 1", "name": "\"Treasure Chest\" by Yuki Tanaka", "video": "https://eudist.animemusicquiz.com/agsvoz.webm", "mp3": "https://eudist.animemusicquiz.com/i3vp9q.mp3"},
-    {"id": 4, "anime": "Akuyaku Reijou Tensei Ojisan - Ending 1", "name": "\"Matsuken Samba II\" by Kazuhiko Inoue, M・A・O", "video": "https://eudist.animemusicquiz.com/e8q47f.webm", "mp3": "https://eudist.animemusicquiz.com/dje0hr.mp3"},
-    {"id": 5, "anime": "Amagami-san Chi no Enmusubi - Ending 2", "name": "\"Kamisama no Iu Toori!\" by ≠ME", "video": "https://eudist.animemusicquiz.com/p4etz1.webm", "mp3": "https://eudist.animemusicquiz.com/wz8z3p.mp3"},
-    {"id": 6, "anime": "Ameku Takao no Suiri Karte - Ending 1", "name": "\"will be fine\" by Gospellers feat. Anly", "video": "https://eudist.animemusicquiz.com/0spf2m.webm", "mp3": "https://eudist.animemusicquiz.com/iagxs7.mp3"},
-    {"id": 7, "anime": "Ao no Exorcist: Yosuga-hen - Ending 1", "name": "\"Overlap\" by Shiyui", "video": "https://eudist.animemusicquiz.com/3z1d8i.webm", "mp3": "https://eudist.animemusicquiz.com/162vdv.mp3"},
-    {"id": 8, "anime": "Ao no Hako - Ending 2", "name": "\"Contrast\" by TOMOO", "video": "https://eudist.animemusicquiz.com/qmcqk3.webm", "mp3": "https://eudist.animemusicquiz.com/l2sgqo.mp3"},
-    {"id": 9, "anime": "Ao no Miburo - Ending 2", "name": "\"Fragment\" by osage", "video": "https://eudist.animemusicquiz.com/8b8iva.webm", "mp3": "https://eudist.animemusicquiz.com/0yt4lm.mp3"},
-    {"id": 10, "anime": "Arafou Otoko no Isekai Tsuuhan - Ending 1", "name": "\"Aicraft\" by Kiminone", "video": "https://eudist.animemusicquiz.com/ohlrpc.webm", "mp3": "https://eudist.animemusicquiz.com/74jbjn.mp3"},
+    {
+        "id": 1,
+        "anime": "Ace of the Diamond Act II - Ending 3",
+        "name": "\"Chance!\" by Suzuko Mimori",
+        "video": "https://naedist.animemusicquiz.com/c86l33.webm",
+        "mp3": "https://naedist.animemusicquiz.com/wqs78c.mp3"
+    },
+    {
+        "id": 2,
+        "anime": "Amanchu! Advance - Opening 1",
+        "name": "\"Crosswalk\" by Minori Suzuki",
+        "video": "https://naedist.animemusicquiz.com/i225y1.webm",
+        "mp3": "https://naedist.animemusicquiz.com/zwjgvu.mp3"
+    },
+    {
+        "id": 3,
+        "anime": "Anti-Magic Academy: The 35th Test Platoon - Ending 1",
+        "name": "\"Calling my Twilight\" by Kanako Itou",
+        "video": "https://naedist.animemusicquiz.com/fn4mep.webm",
+        "mp3": "https://naedist.animemusicquiz.com/13ku9u.mp3"
+    },
+    {
+        "id": 4,
+        "anime": "Ao no Hako - Ending 2",
+        "name": "\"Contrast\" by TOMOO",
+        "video": "https://naedist.animemusicquiz.com/qmcqk3.webm",
+        "mp3": "https://naedist.animemusicquiz.com/l2sgqo.mp3"
+    },
+    {
+        "id": 5,
+        "anime": "Ao no Orchestra - Opening 1",
+        "name": "\"Cantabile\" by Novelbright",
+        "video": "https://naedist.animemusicquiz.com/8276hn.webm",
+        "mp3": "https://naedist.animemusicquiz.com/sgd0j6.mp3"
+    },
+    {
+        "id": 6,
+        "anime": "Aoi Hana - Ending 1",
+        "name": "\"Centifolia\" by Ceui",
+        "video": "https://naedist.animemusicquiz.com/jipaa2.webm",
+        "mp3": "https://naedist.animemusicquiz.com/vr001i.mp3"
+    },
+    {
+        "id": 7,
+        "anime": "BanG Dream! Girls Band Party!: 5th Anniversary Animation - CiRCLE THANKS PARTY! - Ending 1",
+        "name": "\"CiRCLING\" by Poppin'Party",
+        "video": "https://naedist.animemusicquiz.com/0s1878.webm",
+        "mp3": "https://naedist.animemusicquiz.com/59ekh3.mp3"
+    },
+    {
+        "id": 8,
+        "anime": "Candy☆Boy - Ending 1",
+        "name": "\"Candy☆Boy\" by MEILIN",
+        "video": "https://naedist.animemusicquiz.com/0lql4v.webm",
+        "mp3": "https://naedist.animemusicquiz.com/8j6kfs.mp3"
+    },
+    {
+        "id": 9,
+        "anime": "Cardcaptor Sakura: Clear Card-hen - Opening 1",
+        "name": "\"CLEAR\" by Maaya Sakamoto",
+        "video": "https://naedist.animemusicquiz.com/okxltx.webm",
+        "mp3": "https://naedist.animemusicquiz.com/hxt2me.mp3"
+    },
+    {
+        "id": 10,
+        "anime": "Cardfight!! Vanguard Divinez Season 2 - Insert Song",
+        "name": "\"cevl\" by Airots",
+        "video": "https://naedist.animemusicquiz.com/6vs2jw.webm",
+        "mp3": "https://naedist.animemusicquiz.com/3zuvws.mp3"
+    },
+    {
+        "id": 11,
+        "anime": "Chainsaw Man - Ending 1",
+        "name": "\"CHAINSAW BLOOD\" by Vaundy",
+        "video": "https://naedist.animemusicquiz.com/p9bkcr.webm",
+        "mp3": "https://naedist.animemusicquiz.com/31jb1l.mp3"
+    },
+    {
+        "id": 12,
+        "anime": "Code Geass: Hangyaku no Lelouch R2 - Insert Song",
+        "name": "\"Continued Story\" by Hitomi",
+        "video": "https://naedist.animemusicquiz.com/s44tut.webm",
+        "mp3": "https://naedist.animemusicquiz.com/yu4vcq.mp3"
+    },
+    {
+        "id": 13,
+        "anime": "COLORs - Insert Song",
+        "name": "\"COLORs\" by SawanoHiroyuki[nZk]:Motohiro Hata",
+        "video": "https://naedist.animemusicquiz.com/gt25xi.webm",
+        "mp3": "https://naedist.animemusicquiz.com/4b3tvn.mp3"
+    },
+    {
+        "id": 14,
+        "anime": "Cowboy Bebop - Insert Song",
+        "name": "\"CALL ME CALL ME\" by Steve Conte",
+        "video": "https://naedist.animemusicquiz.com/a3xomr.webm",
+        "mp3": "https://naedist.animemusicquiz.com/z6cjgv.mp3"
+    },
+    {
+        "id": 15,
+        "anime": "Darwin's Game - Opening 1",
+        "name": "\"CHAIN\" by ASCA",
+        "video": "https://naedist.animemusicquiz.com/3lhp0c.webm",
+        "mp3": "https://naedist.animemusicquiz.com/bb9b0p.mp3"
+    },
+    {
+        "id": 16,
+        "anime": "Dragon Ball Z: Kami to Kami - Ending 1",
+        "name": "\"CHA-LA HEAD-CHA-LA\" by FLOW",
+        "video": "https://naedist.animemusicquiz.com/ak3326.webm",
+        "mp3": "https://naedist.animemusicquiz.com/w4eh31.mp3"
+    },
+    {
+        "id": 17,
+        "anime": "Durarara!! - Opening 2",
+        "name": "\"Complication\" by ROOKiEZ is PUNK'D",
+        "video": "https://naedist.animemusicquiz.com/pccp95.webm",
+        "mp3": "https://naedist.animemusicquiz.com/6kapiy.mp3"
+    },
+    {
+        "id": 18,
+        "anime": "Eiga Precure Miracle Leap: Minna to Fushigi na 1-nichi - Insert Song",
+        "name": "\"Circle Love ~Sakura~\" by Rie Kitagawa",
+        "video": "https://naedist.animemusicquiz.com/u9h9zg.webm",
+        "mp3": "https://naedist.animemusicquiz.com/k9hf2k.mp3"
+    },
+    {
+        "id": 19,
+        "anime": "Fate/kaleid liner Prisma☆Illya 3rei!! - Ending 2",
+        "name": "\"cuddle\" by ChouCho",
+        "video": "https://naedist.animemusicquiz.com/ke60kp.webm",
+        "mp3": "https://naedist.animemusicquiz.com/r7z1z5.mp3"
+    },
+    {
+        "id": 20,
+        "anime": "Garouden: The Way of the Lone Wolf - Ending 1",
+        "name": "\"CRY BOY\" by AA=",
+        "video": "https://naedist.animemusicquiz.com/olpsk8.webm",
+        "mp3": "https://naedist.animemusicquiz.com/bsl5mm.mp3"
+    },
+    {
+        "id": 21,
+        "anime": "Gekijouban Macross Δ: Gekijou no Walküre - Insert Song",
+        "name": "\"Change!!!!!\" by Walküre",
+        "video": "https://naedist.animemusicquiz.com/4l02u2.webm",
+        "mp3": "https://naedist.animemusicquiz.com/l6nk32.mp3"
+    },
+    {
+        "id": 22,
+        "anime": "Gekijouban Mahou Shoujo Madoka★Magica Shinpen: Hangyaku no Monogatari - Ending 1",
+        "name": "\"Colorful\" by ClariS",
+        "video": "https://naedist.animemusicquiz.com/qn2w0a.webm",
+        "mp3": "https://naedist.animemusicquiz.com/gy15p6.mp3"
+    },
+    {
+        "id": 23,
+        "anime": "Hai to Gensou no Grimgar - Ending 3",
+        "name": "\"Cultivate\" by (K)NoW_NAME:NIKIIE",
+        "video": "https://naedist.animemusicquiz.com/qy8p8r.webm",
+        "mp3": "https://naedist.animemusicquiz.com/9ip858.mp3"
+    },
+    {
+        "id": 24,
+        "anime": "Haikyuu!! Second Season - Ending 1",
+        "name": "\"Climber\" by Galileo Galilei",
+        "video": "https://naedist.animemusicquiz.com/7twva1.webm",
+        "mp3": "https://naedist.animemusicquiz.com/3nc5cv.mp3"
+    },
+    {
+        "id": 25,
+        "anime": "i☆Ris the Movie: Full Energy!! - Insert Song",
+        "name": "\"Cheer up\" by i☆Ris",
+        "video": "https://naedist.animemusicquiz.com/6hm1ab.webm",
+        "mp3": "https://naedist.animemusicquiz.com/owsfwx.mp3"
+    },
+    {
+        "id": 26,
+        "anime": "Kakuriyo no Yadomeshi - Ending 1",
+        "name": "\"color\" by Manami Numakura",
+        "video": "https://naedist.animemusicquiz.com/du3m37.webm",
+        "mp3": "https://naedist.animemusicquiz.com/xde46m.mp3"
+    },
+    {
+        "id": 27,
+        "anime": "Kakushigoto - Opening 1",
+        "name": "\"Chiisana Hibi\" by flumpool",
+        "video": "https://naedist.animemusicquiz.com/c41myn.webm",
+        "mp3": "https://naedist.animemusicquiz.com/pw3ts8.mp3"
+    },
+    {
+        "id": 28,
+        "anime": "Kanojo ga Flag o Oraretara - Opening 1",
+        "name": "\"Cupid Review\" by Aoi Yuuki",
+        "video": "https://naedist.animemusicquiz.com/869jcm.webm",
+        "mp3": "https://naedist.animemusicquiz.com/jbhlnk.mp3"
+    },
+    {
+        "id": 29,
+        "anime": "Kiratto Pri☆Chan - Insert Song",
+        "name": "\"Cutie Breakin'\" Sora Tokui",
+        "video": "https://naedist.animemusicquiz.com/qvgxmh.webm",
+        "mp3": "https://naedist.animemusicquiz.com/x9f5py.mp3"
+    },
+    {
+        "id": 30,
+        "anime": "Love Live! Nijigasaki Gakuen School Idol Doukoukai - Opening 1",
+        "name": "\"Colorful Dreams! Colorful Smiles!\" by Nijigasaki Gakuen School Idol Doukoukai",
+        "video": "https://naedist.animemusicquiz.com/q8g9w3.webm",
+        "mp3": "https://naedist.animemusicquiz.com/i86537.mp3"
+    },
+    {
+        "id": 31,
+        "anime": "Love Live! Nijigasaki High School Idol Club - Insert Song",
+        "name": "\"CHASE!\" by Tomori Kusunoki",
+        "video": "https://naedist.animemusicquiz.com/dl9224.webm",
+        "mp3": "https://naedist.animemusicquiz.com/i0k0lp.mp3"
+    },
+    {
+        "id": 32,
+        "anime": "Love Live! Superstar!! - Insert Song",
+        "name": "\"Chance Day, Chance Way!\" by Liella!",
+        "video": "https://naedist.animemusicquiz.com/whd15r.webm",
+        "mp3": "https://naedist.animemusicquiz.com/ifwj3z.mp3"
+    },
+    {
+        "id": 33,
+        "anime": "Magical Girl Site - Opening 2",
+        "name": "\"Changing point\" by Ryusei Nakao",
+        "video": "https://naedist.animemusicquiz.com/khzme6.webm",
+        "mp3": "https://naedist.animemusicquiz.com/vpspql.mp3"
+    },
+    {
+        "id": 34,
+        "anime": "Make Heroine ga Oosugiru! - Ending 2",
+        "name": "\"CRAZY FOR YOU\" by Shion Wakayama",
+        "video": "https://naedist.animemusicquiz.com/3z4au7.webm",
+        "mp3": "https://naedist.animemusicquiz.com/jleuwe.mp3"
+    },
+    {
+        "id": 35,
+        "anime": "Mobile Suit Gundam Narrative - Insert Song",
+        "name": "\"Cage <NTv>\" by SawanoHiroyuki[nZk]:Tielle",
+        "video": "https://naedist.animemusicquiz.com/mt9qf0.webm",
+        "mp3": "https://naedist.animemusicquiz.com/89lqon.mp3"
+    },
+    {
+        "id": 36,
+        "anime": "Monogatari Series: Off & Monster Season - Opening 2",
+        "name": "\"caramel ribbon cursetard\" by Kana Hanazawa・Saori Hayami",
+        "video": "https://naedist.animemusicquiz.com/h5vezl.webm",
+        "mp3": "https://naedist.animemusicquiz.com/1uhtx5.mp3"
+    },
+    {
+        "id": 37,
+        "anime": "No Guns Life - Opening 1",
+        "name": "\"Chaos Drifters\" by SawanoHiroyuki[nZk]:Jean-Ken Johnny",
+        "video": "https://naedist.animemusicquiz.com/04iulg.webm",
+        "mp3": "https://naedist.animemusicquiz.com/4fevrs.mp3"
+    },
+    {
+        "id": 38,
+        "anime": "One Piece - Opening 28",
+        "name": "\"Carmine\" by ELLEGARDEN",
+        "video": "https://naedist.animemusicquiz.com/cpk6za7n0ir1nx3y.webm",
+        "mp3": "https://naedist.animemusicquiz.com/yu6vap.mp3"
+    },
+    {
+        "id": 39,
+        "anime": "Overlord - Opening 1",
+        "name": "\"Clattanoia\" by OxT",
+        "video": "https://naedist.animemusicquiz.com/gmip23.webm",
+        "mp3": "https://naedist.animemusicquiz.com/3efzdr.mp3"
+    },
+    {
+        "id": 40,
+        "anime": "Paripi Koumei - Opening 1",
+        "name": "\"Chiki Chiki Bang Bang\" by QUEENDOM",
+        "video": "https://naedist.animemusicquiz.com/k0v67f.webm",
+        "mp3": "https://naedist.animemusicquiz.com/4lw4qv.mp3"
+    },
+    {
+        "id": 41,
+        "anime": "Pokémon: Coco\n - Opening 1",
+        "name": "\"Coco\" by Beverly",
+        "video": "https://naedist.animemusicquiz.com/qmdse5.webm",
+        "mp3": "https://naedist.animemusicquiz.com/9z0l0u.mp3"
+    },
+    {
+        "id": 42,
+        "anime": "Puella Magi Madoka Magica - Opening 1",
+        "name": "\"Connect\" by ClariS",
+        "video": "https://naedist.animemusicquiz.com/ao0ex5.webm",
+        "mp3": "https://naedist.animemusicquiz.com/k3y6nh.mp3"
+    },
+    {
+        "id": 43,
+        "anime": "Queen Millennia - Opening 1",
+        "name": "\"Cosmos Dream\" by Masaki Takanashi",
+        "video": "https://naedist.animemusicquiz.com/wkwh0f.webm",
+        "mp3": "https://naedist.animemusicquiz.com/j4qtw1.mp3"
+    },
+    {
+        "id": 44,
+        "anime": "ReLIFE - Ending 2",
+        "name": "\"CHE.R.RY\" by YUI",
+        "video": "https://naedist.animemusicquiz.com/4bhl6c.webm",
+        "mp3": "https://naedist.animemusicquiz.com/59np2h.mp3"
+    },
+    {
+        "id": 45,
+        "anime": "Romeo x Juliet - Ending 1",
+        "name": "“Cyclone” by 12012",
+        "video": "https://naedist.animemusicquiz.com/vg4p3k.webm",
+        "mp3": "https://naedist.animemusicquiz.com/8aeou3.mp3"
+    },
+    {
+        "id": 46,
+        "anime": "RS Keikaku: Rebirth Storage - Ending 1",
+        "name": "\"Crush\" by Pendulum",
+        "video": "https://naedist.animemusicquiz.com/0lai9s.webm",
+        "mp3": "https://naedist.animemusicquiz.com/t2qbcv.mp3"
+    },
+    {
+        "id": 47,
+        "anime": "Sola - Opening 2",
+        "name": "\"colorless wind\" by Aira Yuuki",
+        "video": "https://naedist.animemusicquiz.com/n0th27.webm",
+        "mp3": "https://naedist.animemusicquiz.com/ttlbih.mp3"
+    },
+    {
+        "id": 48,
+        "anime": "Sorcerous Stabber Orphen - Opening 1",
+        "name": "\"Calling U\" by buzz★Vibes",
+        "video": "https://naedist.animemusicquiz.com/1v99z8.webm",
+        "mp3": "https://naedist.animemusicquiz.com/axzw35.mp3"
+    },
+    {
+        "id": 49,
+        "anime": "Soutai Sekai - Ending 1",
+        "name": "\"ChiruChiru\" by REOL",
+        "video": "https://naedist.animemusicquiz.com/s6a064.webm",
+        "mp3": "https://naedist.animemusicquiz.com/374qpj.mp3"
+    },
+    {
+        "id": 50,
+        "anime": "Sword Art Online - Opening 1",
+        "name": "\"crossing field\" by LISA",
+        "video": "https://naedist.animemusicquiz.com/35s57l.webm",
+        "mp3": "https://naedist.animemusicquiz.com/1t8c97.mp3"
+    },
+    {
+        "id": 51,
+        "anime": "Tasogare Otome × Amnesia - Opening 1",
+        "name": "\"Choir Jail\" by Konomi Suzuki",
+        "video": "https://naedist.animemusicquiz.com/f0uo2g.webm",
+        "mp3": "https://naedist.animemusicquiz.com/cz9lyb.mp3"
+    },
+    {
+        "id": 52,
+        "anime": "Tegami Bachi Letter Bee: Reverse - Opening 1",
+        "name": "\"Chiisana Mahou\" by Stereopony",
+        "video": "https://naedist.animemusicquiz.com/czpof7.webm",
+        "mp3": "https://naedist.animemusicquiz.com/qclv7x.mp3"
+    },
+    {
+        "id": 53,
+        "anime": "The God of High School - Opening 1",
+        "name": "\"Contradiction\" by KSUKE feat. Tyler Carter",
+        "video": "https://naedist.animemusicquiz.com/y78m04.webm",
+        "mp3": "https://naedist.animemusicquiz.com/cqyvhq.mp3"
+    },
+    {
+        "id": 54,
+        "anime": "TO-Y - Ending 1",
+        "name": "\"CUBIC LOVERS\" by PSY・S",
+        "video": "https://naedist.animemusicquiz.com/vnncuy.webm",
+        "mp3": "https://naedist.animemusicquiz.com/4chp9n.mp3"
+    },
+    {
+        "id": 55,
+        "anime": "Tonkatsu DJ Agetarou - Ending 1",
+        "name": "\"Check the Back Up\" by Daisuke Fujiwara",
+        "video": "https://naedist.animemusicquiz.com/0112hj.webm",
+        "mp3": "https://naedist.animemusicquiz.com/ivaak9.mp3"
+    },
+    {
+        "id": 56,
+        "anime": "Wolf's Rain - Insert Song",
+        "name": "\"cloud 9\" by maaya sakamoto",
+        "video": "https://naedist.animemusicquiz.com/id3nhk.webm",
+        "mp3": "https://naedist.animemusicquiz.com/3kn465.mp3"
+    },
 ] satisfies Song[];
