13a71c9673cce9f18f2e7fdebe090eb43cb87e87
amq-tutti <tutti-amq@proton.me>
2026-07-30 15:26:10 +0300

    Create "Takagi-san Original Songs" sorter

--- customize/songList.ts
+++ customize/songList.ts
@@ -3,14 +3,200 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {"id": 1, "anime": "0-saiji Start Dash Monogatari Season 2 - Ending 1", "name": "\"Beyond the Trajectory\" by Chata", "video": "https://eudist.animemusicquiz.com/5u2mhk.webm", "mp3": null},
-    {"id": 2, "anime": "1-punkan dake Furete mo Ii yo... Share House no Himitsu Rule. - Ending 1", "name": "\"Session Sadame Night\" by Dai Takanashi", "video": "https://eudist.animemusicquiz.com/f23gq2.webm", "mp3": null},
-    {"id": 3, "anime": "A-Rank Party wo Ridatsu shita Ore wa, Moto Oshiego-tachi to Meikyuu Shinbu wo Mezasu. - Ending 1", "name": "\"Treasure Chest\" by Yuki Tanaka", "video": "https://eudist.animemusicquiz.com/agsvoz.webm", "mp3": "https://eudist.animemusicquiz.com/i3vp9q.mp3"},
-    {"id": 4, "anime": "Akuyaku Reijou Tensei Ojisan - Ending 1", "name": "\"Matsuken Samba II\" by Kazuhiko Inoue, M・A・O", "video": "https://eudist.animemusicquiz.com/e8q47f.webm", "mp3": "https://eudist.animemusicquiz.com/dje0hr.mp3"},
-    {"id": 5, "anime": "Amagami-san Chi no Enmusubi - Ending 2", "name": "\"Kamisama no Iu Toori!\" by ≠ME", "video": "https://eudist.animemusicquiz.com/p4etz1.webm", "mp3": "https://eudist.animemusicquiz.com/wz8z3p.mp3"},
-    {"id": 6, "anime": "Ameku Takao no Suiri Karte - Ending 1", "name": "\"will be fine\" by Gospellers feat. Anly", "video": "https://eudist.animemusicquiz.com/0spf2m.webm", "mp3": "https://eudist.animemusicquiz.com/iagxs7.mp3"},
-    {"id": 7, "anime": "Ao no Exorcist: Yosuga-hen - Ending 1", "name": "\"Overlap\" by Shiyui", "video": "https://eudist.animemusicquiz.com/3z1d8i.webm", "mp3": "https://eudist.animemusicquiz.com/162vdv.mp3"},
-    {"id": 8, "anime": "Ao no Hako - Ending 2", "name": "\"Contrast\" by TOMOO", "video": "https://eudist.animemusicquiz.com/qmcqk3.webm", "mp3": "https://eudist.animemusicquiz.com/l2sgqo.mp3"},
-    {"id": 9, "anime": "Ao no Miburo - Ending 2", "name": "\"Fragment\" by osage", "video": "https://eudist.animemusicquiz.com/8b8iva.webm", "mp3": "https://eudist.animemusicquiz.com/0yt4lm.mp3"},
-    {"id": 10, "anime": "Arafou Otoko no Isekai Tsuuhan - Ending 1", "name": "\"Aicraft\" by Kiminone", "video": "https://eudist.animemusicquiz.com/ohlrpc.webm", "mp3": "https://eudist.animemusicquiz.com/74jbjn.mp3"},
+    {
+        "id": 1,
+        "anime": "Karakai Jouzu no Takagi-san - Original ED1",
+        "name": "\"Kimagure Romantic\" by Ikimono-gakari",
+        "video": "https://youtu.be/5XCSt_0lwOE?si=KNtFi3Gos8EnCcJ-",
+        "mp3": null
+    },
+    {
+        "id": 2,
+        "anime": "Karakai Jouzu no Takagi-san - Original ED2",
+        "name": "\"AM11:00\" by HY",
+        "video": "https://youtu.be/-eVcW-b1ITA?si=rpDHqCDfpckmmTBf",
+        "mp3": null
+    },
+    {
+        "id": 3,
+        "anime": "Karakai Jouzu no Takagi-san - Original ED3",
+        "name": "\"Jitensha\" by JUDY AND MARY",
+        "video": "https://youtu.be/yoZknTngtlc?si=CgT0p4ZwTcnDgoB0",
+        "mp3": null
+    },
+    {
+        "id": 4,
+        "anime": "Karakai Jouzu no Takagi-san - Original ED4",
+        "name": "\"Kaze Fukeba Koi\" by Chatmonchy",
+        "video": "https://youtu.be/HNzTHyNX3yw?si=EgvPrWniZ-Mg1K-A",
+        "mp3": null
+    },
+    {
+        "id": 5,
+        "anime": "Karakai Jouzu no Takagi-san - Original ED5",
+        "name": "\"Chiisana Koi no Uta\" by MONGOL800",
+        "video": "https://youtu.be/JoJ6lnF07U4?si=bbobv3ehwTemKZLv",
+        "mp3": null
+    },
+    {
+        "id": 6,
+        "anime": "Karakai Jouzu no Takagi-san - Original ED6",
+        "name": "\"Ai Uta\" by GReeeeN",
+        "video": "https://youtu.be/EvwRYGlJWfQ?si=x4LPCcTbC1nLJUs_",
+        "mp3": null
+    },
+    {
+        "id": 7,
+        "anime": "Karakai Jouzu no Takagi-san - Original ED7",
+        "name": "\"Deatta Koro no You ni\" by Every Little Thing",
+        "video": "https://youtu.be/k1S0i8FoV4c?si=9GmtwwDtc6-sWe7J",
+        "mp3": null
+    },
+    {
+        "id": 8,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original ED1",
+        "name": "\"Kanade\" by Sukima Switch",
+        "video": "https://youtu.be/J5Z7tIq7bco?si=ttVw_AZinmsjPqb_",
+        "mp3": null
+    },
+    {
+        "id": 9,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original ED2",
+        "name": "\"Konayuki\" by Remioromen",
+        "video": "https://youtu.be/1wxTksLZ1Mw?si=jVCRJbDp-mDT3dH7",
+        "mp3": null
+    },
+    {
+        "id": 10,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original ED3",
+        "name": "\"Kiseki\" by GReeeeN",
+        "video": "https://youtu.be/tO5KPGgwb-I?si=S6Rf9xTwIQyweD3M",
+        "mp3": null
+    },
+    {
+        "id": 11,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original ED4",
+        "name": "\"Arigatou\" by Ikimono-gakari",
+        "video": "https://youtu.be/VZBU8LvZ91Q?si=4n3c7vDJRc9epV7F",
+        "mp3": null
+    },
+    {
+        "id": 12,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original ED5",
+        "name": "\"STARS\" by Mika Nakashima",
+        "video": "https://youtu.be/hJBHMUGI7Nc?si=EZFZW1GfgQRwdYd7",
+        "mp3": null
+    },
+    {
+        "id": 13,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original ED6",
+        "name": "\"Anata ni\" by MONGOL800",
+        "video": "https://youtu.be/lWbJFp_OSSg?si=5wPGobHhQb0xMVzQ",
+        "mp3": null
+    },
+    {
+        "id": 14,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original ED7",
+        "name": "\"Iwanai kedo ne.\" by Yuiko Ohara",
+        "video": "https://youtu.be/on1xXUajD1o?si=vLuWQzv_XRM6uKZi",
+        "mp3": null
+    },
+    {
+        "id": 15,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original ED8",
+        "name": "\"Yasashii Kimochi\" by Chara",
+        "video": "https://youtu.be/w1QJ56BINLw?si=5vUjTC_k9p_2SnuO",
+        "mp3": null
+    },
+    {
+        "id": 16,
+        "anime": "Karakai Jouzu no Takagi-san 2 - Original Image Song",
+        "name": "\"Hyakuman-kai no 'I Love You'\" by Rake",
+        "video": "https://youtu.be/alkESkHlEb0?si=qp9oFdBcdmk_KTf1",
+        "mp3": null
+    },
+    {
+        "id": 17,
+        "anime": "Karakai Jouzu no Takagi-san 3 - Original ED1",
+        "name": "\"Yume de Aetara\" by Eiichi Ohtaki",
+        "video": "https://youtu.be/NU0FrS0eOwM?si=VSnTF-UcXiVlNzUW",
+        "mp3": null
+    },
+    {
+        "id": 18,
+        "anime": "Karakai Jouzu no Takagi-san 3 - Original ED2",
+        "name": "\"Over Drive\" by JUDY AND MARY",
+        "video": "https://youtu.be/FfCP2Q0srMs?si=cTqg7FLImygyCqkK",
+        "mp3": null
+    },
+    {
+        "id": 19,
+        "anime": "Karakai Jouzu no Takagi-san 3 - Original ED3",
+        "name": "\"Himawari no Yakusoku\" by Motohiro Hata",
+        "video": "https://youtu.be/rKsQ-3N-Bks?si=-MUCmE9HC21XIkzV",
+        "mp3": null
+    },
+    {
+        "id": 20,
+        "anime": "Karakai Jouzu no Takagi-san 3 - Original ED4",
+        "name": "\"Gakuen Tengoku\" by Finger 5",
+        "video": "https://youtu.be/f29bC7ZB19g?si=F_xCDMaP6_v_Dvmx",
+        "mp3": null
+    },
+    {
+        "id": 21,
+        "anime": "Karakai Jouzu no Takagi-san 3 - Original ED5",
+        "name": "\"Joyful\" by Ikimono-gakari",
+        "video": "https://youtu.be/viQPExRmxdk?si=m4rn1PWkUGTT6RoW",
+        "mp3": null
+    },
+    {
+        "id": 22,
+        "anime": "Karakai Jouzu no Takagi-san 3 - Original ED6",
+        "name": "\"Santa Claus Is Comin' to Town\" by Bing Crosby and The Andrews Sisters",
+        "video": "https://youtu.be/SLPIPCEcDOc?si=wYvEG8srYhcT-sHL",
+        "mp3": null
+    },
+    {
+        "id": 23,
+        "anime": "Karakai Jouzu no Takagi-san 3 - Original ED7",
+        "name": "\"Snow Magic Fantasy\" by SEKAI NO OWARI",
+        "video": "https://youtu.be/vkcTN70fnXA?si=Meole2Bsk1D3C1vH",
+        "mp3": null
+    },
+    {
+        "id": 24,
+        "anime": "Karakai Jouzu no Takagi-san 3 - Original ED8",
+        "name": "\"Hana\" by ORANGE RANGE",
+        "video": "https://youtu.be/iYkg90hDInc?si=7vhdasxWNICXcVpA",
+        "mp3": null
+    },
+    {
+        "id": 25,
+        "anime": "Gekijouban Karakai Jouzu no Takagi-san - Original ED1",
+        "name": "\"Asu e no Tobira\" by I WiSH",
+        "video": "https://youtu.be/QIBPUU_tuJE?si=U2NNcdU_DYI3fpyZ",
+        "mp3": null
+    },
+    {
+        "id": 26,
+        "anime": "Gekijouban Karakai Jouzu no Takagi-san - Original ED2",
+        "name": "\"Tentai Kansoku\" by BUMP OF CHICKEN",
+        "video": "https://youtu.be/j7CDb610Bg0?si=1MUnHgSRdqgmfC9S",
+        "mp3": null
+    },
+    {
+        "id": 27,
+        "anime": "Gekijouban Karakai Jouzu no Takagi-san - Original ED3",
+        "name": "\"fragile\" by Every Little Thing",
+        "video": "https://youtu.be/NP60Qqjfdhs?si=n5YTMV7mHBOj5cqX",
+        "mp3": null
+    },
+    {
+        "id": 28,
+        "anime": "Gekijouban Karakai Jouzu no Takagi-san - Original ED4",
+        "name": "\"Kimi ni Todoke\" by flumpool",
+        "video": "https://youtu.be/ZNu10SawQgc?si=6ZT2q3qa9Wd3LDXc",
+        "mp3": null
+    },
 ] satisfies Song[];
