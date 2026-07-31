891551fb4de09d35c434e9838d9565c8bd51a472
amq-tutti <tutti-amq@proton.me>
2026-07-31 16:20:24 +0300

    Create Asaka sorter

--- customize/songList.ts
+++ customize/songList.ts
@@ -3,14 +3,340 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {"id": 1, "anime": "0-saiji Start Dash Monogatari Season 2 - Ending 1", "name": "\"Beyond the Trajectory\" by Chata", "video": "https://eudist.animemusicquiz.com/5u2mhk.webm", "mp3": null},
-    {"id": 2, "anime": "1-punkan dake Furete mo Ii yo... Share House no Himitsu Rule. - Ending 1", "name": "\"Session Sadame Night\" by Dai Takanashi", "video": "https://eudist.animemusicquiz.com/f23gq2.webm", "mp3": null},
-    {"id": 3, "anime": "A-Rank Party wo Ridatsu shita Ore wa, Moto Oshiego-tachi to Meikyuu Shinbu wo Mezasu. - Ending 1", "name": "\"Treasure Chest\" by Yuki Tanaka", "video": "https://eudist.animemusicquiz.com/agsvoz.webm", "mp3": "https://eudist.animemusicquiz.com/i3vp9q.mp3"},
-    {"id": 4, "anime": "Akuyaku Reijou Tensei Ojisan - Ending 1", "name": "\"Matsuken Samba II\" by Kazuhiko Inoue, M・A・O", "video": "https://eudist.animemusicquiz.com/e8q47f.webm", "mp3": "https://eudist.animemusicquiz.com/dje0hr.mp3"},
-    {"id": 5, "anime": "Amagami-san Chi no Enmusubi - Ending 2", "name": "\"Kamisama no Iu Toori!\" by ≠ME", "video": "https://eudist.animemusicquiz.com/p4etz1.webm", "mp3": "https://eudist.animemusicquiz.com/wz8z3p.mp3"},
-    {"id": 6, "anime": "Ameku Takao no Suiri Karte - Ending 1", "name": "\"will be fine\" by Gospellers feat. Anly", "video": "https://eudist.animemusicquiz.com/0spf2m.webm", "mp3": "https://eudist.animemusicquiz.com/iagxs7.mp3"},
-    {"id": 7, "anime": "Ao no Exorcist: Yosuga-hen - Ending 1", "name": "\"Overlap\" by Shiyui", "video": "https://eudist.animemusicquiz.com/3z1d8i.webm", "mp3": "https://eudist.animemusicquiz.com/162vdv.mp3"},
-    {"id": 8, "anime": "Ao no Hako - Ending 2", "name": "\"Contrast\" by TOMOO", "video": "https://eudist.animemusicquiz.com/qmcqk3.webm", "mp3": "https://eudist.animemusicquiz.com/l2sgqo.mp3"},
-    {"id": 9, "anime": "Ao no Miburo - Ending 2", "name": "\"Fragment\" by osage", "video": "https://eudist.animemusicquiz.com/8b8iva.webm", "mp3": "https://eudist.animemusicquiz.com/0yt4lm.mp3"},
-    {"id": 10, "anime": "Arafou Otoko no Isekai Tsuuhan - Ending 1", "name": "\"Aicraft\" by Kiminone", "video": "https://eudist.animemusicquiz.com/ohlrpc.webm", "mp3": "https://eudist.animemusicquiz.com/74jbjn.mp3"},
+    {
+        "id": 1,
+        "anime": "A lot of Stories - IN",
+        "name": "\"ILLUMINA\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=qxurYI21iWk",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=qxurYI21iWk"
+    },
+    {
+        "id": 2,
+        "anime": "ANONYMOUS;CODE - ED2",
+        "name": "\"Mob's God's\" by Asaka",
+        "video": "https://youtu.be/0W1UJEy-ZyU?si=6fi4PFAmi8WC0y8t&t=100",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=C2egw9XGx6c"
+    },
+    {
+        "id": 3,
+        "anime": "Arafou Otoko no Isekai Tsuuhan - OP",
+        "name": "\"GIVE & TAKE\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/36aebe.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=9jPECdrT0Mk"
+    },
+    {
+        "id": 4,
+        "anime": "Centaur no Nayami - ED",
+        "name": "\"Edelweiss\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/y6a8p4.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=1GdivZ_DLa4"
+    },
+    {
+        "id": 5,
+        "anime": "Centaur no Nayami - EP9 ED",
+        "name": "\"Edelweiss (English Version)\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/5pm2t4.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=gG5_hqkU61M"
+    },
+    {
+        "id": 6,
+        "anime": "Choujigen Game Neptune: Hidamari no Little Purple - IN",
+        "name": "\"Never ending true stories\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/83wda3.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=sfNJHwISXnw"
+    },
+    {
+        "id": 7,
+        "anime": "CODE:SEED -Seika no Uta- - OP",
+        "name": "\"Getsuuntan-lunaring-\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=61eKQhca4tI",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=y-b3HHwyv8c"
+    },
+    {
+        "id": 8,
+        "anime": "Collar×Malice -Unlimited- - ED1",
+        "name": "\"CITYSCAPE\" by Asaka",
+        "video": "https://youtu.be/s44GTEWDXHc?si=UFBmhenmOXoFnHaH&t=544",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=PSxhfxp2FlY"
+    },
+    {
+        "id": 9,
+        "anime": "Corpse Party II - Darkness Distortion - ED",
+        "name": "\"Hakanai Rensa\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=-oP-wpPLxMg",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=-oP-wpPLxMg"
+    },
+    {
+        "id": 10,
+        "anime": "Daikenja Ryddel no Jikan Gyakkou - ED",
+        "name": "\"Savage\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/n7a744.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=P4hJ4f9GstM"
+    },
+    {
+        "id": 11,
+        "anime": "Dosanko Gal wa Namaramenkoi - ED",
+        "name": "\"Wayawayawaa!\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/l0x6jm.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=C9ehiV9XkFY"
+    },
+    {
+        "id": 12,
+        "anime": "Eiga Yuru Camp△ - OP",
+        "name": "\"Sun Is Coming Up\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/bmvpvz.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=7IT-dGcHptA"
+    },
+    {
+        "id": 13,
+        "anime": "Grisaia Phantom Trigger Vol. 6 - ED",
+        "name": "\"Childhood's End\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=80j59gEioCA",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=80j59gEioCA"
+    },
+    {
+        "id": 14,
+        "anime": "Heya Camp△ - ED",
+        "name": "\"The Sunshower\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/hkvfxt.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=7a03ysUTXXg"
+    },
+    {
+        "id": 15,
+        "anime": "Higurashi no Naku Koro ni Gou - OP",
+        "name": "\"I believe what you said\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/hhh0q3.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=dNxDdQw91iE"
+    },
+    {
+        "id": 16,
+        "anime": "Hyperdimension Neptunia GameMaker R:Evolution - ED",
+        "name": "\"Trust My Heart\" by Asaka",
+        "video": "https://youtu.be/i8CvPBtmEks?si=DLTtVC7cZadXiJmB&t=1453",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=W8_X2Tc9GWI"
+    },
+    {
+        "id": 17,
+        "anime": "Island - ED1",
+        "name": "\"Eternal Star\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/ryqt28.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=X-l6DRXDe5M"
+    },
+    {
+        "id": 18,
+        "anime": "Island - ED2",
+        "name": "\"Marine SNOW\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/thw4co.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=C_MxeI3MeFo"
+    },
+    {
+        "id": 19,
+        "anime": "Kenja no Deshi wo Nanoru Kenja - OP",
+        "name": "\"Ready Set Go!!\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/6zjw7k.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=sCcvuxqHvV8"
+    },
+    {
+        "id": 20,
+        "anime": "Kenja no Deshi wo Nanoru Kenja - EP8 ED",
+        "name": "\"Ballad\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/khqhia.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=mj6_CHRCyAA"
+    },
+    {
+        "id": 21,
+        "anime": "Kono Yo no Hate de Koi o Utau Shoujo YU-NO - OP1",
+        "name": "\"Kono Yo no Hate de Koi wo Utau Shoujo\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/rc672w.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=E72ivJl0JcY"
+    },
+    {
+        "id": 22,
+        "anime": "Kono Yo no Hate de Koi o Utau Shoujo YU-NO - ED2",
+        "name": "\"Kami no Suushiki\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/5a34xd.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=qvCbIBG3d8g"
+    },
+    {
+        "id": 23,
+        "anime": "Megadimension Neptunia VIIR - ED",
+        "name": "\"Kimi to Hajimaru Monogatari\" by Asaka",
+        "video": "https://youtu.be/IeQTMAdY50o?si=AgsOppp2ema8G4S9&t=623",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=vxBz5mYQ5dk"
+    },
+    {
+        "id": 24,
+        "anime": "Meiji Katsugeki Haikara Ryuuseigumi -Seibai Shimaseu, Yonaoshi Hagyou- - ED",
+        "name": "\"Ryuusei no Sora e\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=mhWrNmKjuXg",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=mhWrNmKjuXg"
+    },
+    {
+        "id": 25,
+        "anime": "Mystonia no Kibou -The Lost Delight- - ED1",
+        "name": "\"White Aube\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=Un8tuG9RNDI",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=Un8tuG9RNDI"
+    },
+    {
+        "id": 26,
+        "anime": "Mystonia no Kibou -The Lost Delight- - ED2",
+        "name": "\"I Don't Even Know...\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=Un8tuG9RNDI",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=p-ephd3y32A"
+    },
+    {
+        "id": 27,
+        "anime": "Naka no Hito Genome [Jikkyouchuu] - EP9 IN",
+        "name": "\"Code \"Genius\"?\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/wvgfrn.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=cWFVB0NTKQU"
+    },
+    {
+        "id": 28,
+        "anime": "Neptunia x SENRAN KAGURA: Ninja Wars - OP",
+        "name": "\"Utae Odore, Otome wa Tsuyoshi.\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=LOb4iYMuKL4",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=9Yz4jMK67Jw"
+    },
+    {
+        "id": 29,
+        "anime": "Occultic;Nine - ED",
+        "name": "\"Open your eyes\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/fnwh8f.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=Mhy5VvEeES4"
+    },
+    {
+        "id": 30,
+        "anime": "Occultic;Nine (Game) - OP",
+        "name": "\"Play the game\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=4-NFMahhOVE",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=tAeMgWEiO-A"
+    },
+    {
+        "id": 31,
+        "anime": "Rear Sekai - OP",
+        "name": "\"All is mind!\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=Jx3rT099o3M",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=luBAiNT3fDw"
+    },
+    {
+        "id": 32,
+        "anime": "Shikizakura - OP",
+        "name": "\"BELIEVE MYSELF\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/rliihj.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=63iAXMEBlOc"
+    },
+    {
+        "id": 33,
+        "anime": "SINce Memories: Hoshi no Sora no Shita - OP",
+        "name": "\"Hikari to Kage no Laplace\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=IgyA9HD6tPQ",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=-N9AhNmiRBw"
+    },
+    {
+        "id": 34,
+        "anime": "SUMMER TIME RENDERING: Another Horizon - OP",
+        "name": "\"Touka\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=f0FsWCqZOZo",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=Ov-K_2CrazM"
+    },
+    {
+        "id": 35,
+        "anime": "Summertime Render - OP2",
+        "name": "\"Natsuyume Noisy\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/r4wrc2.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=b7VeYS_fxdY"
+    },
+    {
+        "id": 36,
+        "anime": "Utagoe wa Mille-Feuille - ED",
+        "name": "\"Risky Business\" by Parabola",
+        "video": "https://naedist.animemusicquiz.com/8emfcoc9wurgs6c0.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=QADFU9zSmtk"
+    },
+    {
+        "id": 37,
+        "anime": "Yumeutsutsu Re:Master - OP",
+        "name": "\"Place of promise\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=2gkMQ3pde5o",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=28mUcZ_EotE"
+    },
+    {
+        "id": 38,
+        "anime": "Yuru Camp△ - OP",
+        "name": "\"SHINY DAYS\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/1pj273.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=rFMo9PULMtk"
+    },
+    {
+        "id": 39,
+        "anime": "Yuru Camp△ Have a nice day! - OP",
+        "name": "\"See The Light\" by Asaka",
+        "video": "https://www.youtube.com/watch?v=_jiaHU2fu0Y",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=H_cnIyr3Vj4"
+    },
+    {
+        "id": 40,
+        "anime": "Yuru Camp△ Season 2 - OP",
+        "name": "\"Seize The Day\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/0bl6rq.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=eKo6yEE3oCA"
+    },
+    {
+        "id": 41,
+        "anime": "Yuru Camp△ Season 2 - EP7 IN",
+        "name": "\"Kono Basho de.\" by Okunai Katsudou Circle (Asaka)",
+        "video": "https://naedist.animemusicquiz.com/yif5sw.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=It4am72mDfM"
+    },
+    {
+        "id": 42,
+        "anime": "Yuru Camp△ Season 3 - ED",
+        "name": "\"So Precious\" by Asaka",
+        "video": "https://naedist.animemusicquiz.com/ot6lvc.webm",
+        "mp3": null,
+        "full": "https://www.youtube.com/watch?v=vYuvUKc1XPM"
+    },
 ] satisfies Song[];
