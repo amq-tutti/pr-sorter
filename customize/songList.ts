2dddb5f512ff08deb5c51d64ba0ba74ca3f4f92f
amq-tutti <tutti-amq@proton.me>
2026-08-28 00:08:28 +0300

    Update song ID 77 full again

--- customize/songList.ts
+++ customize/songList.ts
@@ -617,7 +617,7 @@ export const songList = [
         "name": "\"Obake Zukan no Uta ~Itsudemo Dokodemo~\" by Nana Mizuki",
         "video": "https://naedist.animemusicquiz.com/vvflne.webm",
         "mp3": null,
-        "full": "https://files.catbox.moe/597i5h.webm"
+        "full": "https://files.catbox.moe/mshg5r.webm"
     },
     {
         "id": 78,
