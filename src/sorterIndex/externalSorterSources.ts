53f4e50b719126b7ae12aecce1525cb01f1070cf
amq-tutti <tutti-amq@proton.me>
2026-09-04 03:25:57 +0300

    Add Shuuka as external sorter source

--- src/sorterIndex/externalSorterSources.ts
+++ src/sorterIndex/externalSorterSources.ts
@@ -6,4 +6,9 @@ export const externalSorterSources = [
         indexUrl: 'https://minigamer42.github.io/pr-sorter/',
         excludedSorterSlugs: ['yorushika', 'princession', 'suara', 'yuuko-natsuyoshi'],
     },
+    {
+        title: 'Shuuka',
+        indexUrl: 'https://shuukaa.github.io/pr-sorter/',
+        excludedSorterSlugs: [],
+    },
 ] satisfies ExternalSorterSource[];
