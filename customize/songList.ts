6fb6d124a0708adcc968fe63db18331140db3d8b
amq-tutti <tutti-amq@proton.me>
2026-09-09 17:32:09 +0300

    Add " " around song names

--- customize/songList.ts
+++ customize/songList.ts
@@ -6,77 +6,77 @@ export const songList = [
     {
         "id": 1,
         "anime": "Appleseed - ED1",
-        "name": "Crystal Celebration by Risa Youki",
+        "name": "\"Crystal Celebration\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/avu2st.webm",
         "mp3": "https://eudist.animemusicquiz.com/3gmj09.mp3"
     },
     {
         "id": 2,
         "anime": "Akai Koudan Zillion - OP1",
-        "name": "Pure Stone by Risa Youki",
+        "name": "\"Pure Stone\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/h9wu84.webm",
         "mp3": "https://eudist.animemusicquiz.com/k84zcc.mp3"
     },
     {
         "id": 3,
         "anime": "Akai Koudan Zillion - ED1",
-        "name": "Push! by Risa Youki",
+        "name": "\"Push!\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/t2ah55.webm",
         "mp3": "https://eudist.animemusicquiz.com/l4mjfo.mp3"
     },
     {
         "id": 4,
         "anime": "Akai Koudan Zillion - ED2",
-        "name": "Rock Candy by Risa Youki",
+        "name": "\"Rock Candy\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/53fi5u.webm",
         "mp3": "https://eudist.animemusicquiz.com/7bubho.mp3"
     },
     {
         "id": 5,
         "anime": "Akai Koudan Zillion - IN",
-        "name": "No More Normal by Risa Youki",
+        "name": "\"No More Normal\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/7mgg47.webm",
         "mp3": "https://eudist.animemusicquiz.com/87r7ss.mp3"
     },
     {
         "id": 6,
         "anime": "Tenkuu Senki Shurato - IN",
-        "name": "Yasashisa no Mukougawa by Risa Youki",
+        "name": "\"Yasashisa no Mukougawa\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/7on6pe.webm",
         "mp3": "https://eudist.animemusicquiz.com/orn8pc.mp3"
     },
     {
         "id": 7,
         "anime": "Akai Koudan Zillion: Utahime Yakyoku - OP1",
-        "name": "Nageki no KIDS by Risa Youki",
+        "name": "\"Nageki no KIDS\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/n6dkqs.webm",
         "mp3": "https://eudist.animemusicquiz.com/5ay21y.mp3"
     },
     {
         "id": 8,
         "anime": "Akai Koudan Zillion: Utahime Yakyoku - ED1",
-        "name": "Sayonara no Machikado by Risa Youki",
+        "name": "\"Sayonara no Machikado\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/95r6nj.webm",
         "mp3": "https://eudist.animemusicquiz.com/6mtfmk.mp3"
     },
     {
         "id": 9,
         "anime": "Kidou Senshi SD Gundam Mk-II - ED1",
-        "name": "Planet Zone by Risa Youki",
+        "name": "\"Planet Zone\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/3vqls8.webm",
         "mp3": "https://eudist.animemusicquiz.com/xl07nj.mp3"
     },
     {
         "id": 10,
         "anime": "B.B.: Burning Blood - OP1",
-        "name": "BURNING BLOOD by Risa Youki",
+        "name": "\"BURNING BLOOD\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/4h8yta.webm",
         "mp3": "https://eudist.animemusicquiz.com/mfxuda.mp3"
     },
     {
         "id": 11,
         "anime": "B.B.: Burning Blood - ED1",
-        "name": "BELIEVE IN by Risa Youki",
+        "name": "\"BELIEVE IN\" by Risa Youki",
         "video": "https://eudist.animemusicquiz.com/k86o04.webm",
         "mp3": "https://eudist.animemusicquiz.com/si39gu.mp3"
     }
