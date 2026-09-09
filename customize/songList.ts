03094719cee892e63e431065fb01b6e5092569c7
amq-tutti <tutti-amq@proton.me>
2026-09-09 17:31:34 +0300

    Add " " around song names

--- customize/songList.ts
+++ customize/songList.ts
@@ -6,91 +6,91 @@ export const songList = [
     {
         "id": 1,
         "anime": "Princess Nine: Kisaragi Joshikou Yakyuu-bu - OP1",
-        "name": "Princess Nine by Miki Nagasawa & Mami Kingetsu",
+        "name": "\"Princess Nine\" by Miki Nagasawa & Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/u34u0t.webm",
         "mp3": "https://eudist.animemusicquiz.com/yhtwb0.mp3"
     },
     {
         "id": 2,
         "anime": "Princess Nine: Kisaragi Joshikou Yakyuu-bu - ED1",
-        "name": "PASSIONATE DAYS by Miki Nagasawa & Mami Kingetsu",
+        "name": "\"PASSIONATE DAYS\" by Miki Nagasawa & Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/166rl5.webm",
         "mp3": "https://eudist.animemusicquiz.com/dqm2ee.mp3"
     },
     {
         "id": 3,
         "anime": "Princess Nine: Kisaragi Joshikou Yakyuu-bu - IN",
-        "name": "Spring rainy day by Mami Kingetsu",
+        "name": "\"Spring rainy day\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/3yggle.webm",
         "mp3": "https://eudist.animemusicquiz.com/0jjqlc.mp3"
     },
     {
         "id": 4,
         "anime": "Princess Nine: Kisaragi Joshikou Yakyuu-bu - IN",
-        "name": "Dream Stadium by Mami Kingetsu",
+        "name": "\"Dream Stadium\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/3u5dnw.webm",
         "mp3": "https://eudist.animemusicquiz.com/zys6tz.mp3"
     },
     {
         "id": 5,
         "anime": "Burn Up Excess - OP1",
-        "name": "SHOW TIME by Mami Kingetsu",
+        "name": "\"SHOW TIME\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/3be37q.webm",
         "mp3": "https://eudist.animemusicquiz.com/xf99r4.mp3"
     },
     {
         "id": 6,
         "anime": "Yume de Aetara - OP1",
-        "name": "Ai Suru Koto o Oshiete by Mami Kingetsu",
+        "name": "\"Ai Suru Koto o Oshiete\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/y0fd4s.webm",
         "mp3": "https://eudist.animemusicquiz.com/mp01ue.mp3"
     },
     {
         "id": 7,
         "anime": "Yume de Aetara - ED1",
-        "name": "DEAR by Mami Kingetsu",
+        "name": "\"DEAR\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/834znu.webm",
         "mp3": "https://eudist.animemusicquiz.com/8r3nni.mp3"
     },
     {
         "id": 8,
         "anime": "Tokimeki Memorial: Forever With You - OP1",
-        "name": "Motto! Motto! Tokimeki by Mami Kingetsu",
+        "name": "\"Motto! Motto! Tokimeki\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/1785wx.webm",
         "mp3": "https://eudist.animemusicquiz.com/daiz87.mp3"
     },
     {
         "id": 9,
         "anime": "Tokimeki Memorial: Forever With You - OP2",
-        "name": "Beginning by Mami Kingetsu",
+        "name": "\"Beginning\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/paahkv.webm",
         "mp3": "https://eudist.animemusicquiz.com/sdjiey.mp3"
     },
     {
         "id": 10,
         "anime": "Tokimeki Memorial: Forever With You - ED1",
-        "name": "MAYBE LOVE by Mami Kingetsu",
+        "name": "\"MAYBE LOVE\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/7ht2ic.webm",
         "mp3": "https://eudist.animemusicquiz.com/zd57ij.mp3"
     },
     {
         "id": 11,
         "anime": "Yume de Aetara - IN",
-        "name": "MAYBE LOVE by Mami Kingetsu",
+        "name": "\"MAYBE LOVE\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/a2acl1.webm",
         "mp3": "https://eudist.animemusicquiz.com/yzmyg9.mp3"
     },
     {
         "id": 12,
         "anime": "Galaxy Angel X - IN",
-        "name": "Motto! Angel by Mami Kingetsu",
+        "name": "\"Motto! Angel\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/l76sav.webm",
         "mp3": "https://eudist.animemusicquiz.com/eh24yd.mp3"
     },
     {
         "id": 13,
         "anime": "Generation of Chaos - ED1",
-        "name": "Kono Sora no Mukou by Mami Kingetsu",
+        "name": "\"Kono Sora no Mukou\" by Mami Kingetsu",
         "video": "https://eudist.animemusicquiz.com/set2gx.webm",
         "mp3": "https://eudist.animemusicquiz.com/iqm961.mp3"
     },
