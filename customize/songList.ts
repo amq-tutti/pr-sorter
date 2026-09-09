2820db672fe377c493d29177e726fe42efbb413f
amq-tutti <tutti-amq@proton.me>
2026-09-09 17:27:34 +0300

    Create Risa Youki sorter

--- customize/songList.ts
+++ customize/songList.ts
@@ -3,14 +3,81 @@
 import type { Song } from '../src/songs';
 
 export const songList = [
-    {"id": 1, "anime": "0-saiji Start Dash Monogatari Season 2 - Ending 1", "name": "\"Beyond the Trajectory\" by Chata", "video": "https://eudist.animemusicquiz.com/5u2mhk.webm", "mp3": null},
-    {"id": 2, "anime": "1-punkan dake Furete mo Ii yo... Share House no Himitsu Rule. - Ending 1", "name": "\"Session Sadame Night\" by Dai Takanashi", "video": "https://eudist.animemusicquiz.com/f23gq2.webm", "mp3": null},
-    {"id": 3, "anime": "A-Rank Party wo Ridatsu shita Ore wa, Moto Oshiego-tachi to Meikyuu Shinbu wo Mezasu. - Ending 1", "name": "\"Treasure Chest\" by Yuki Tanaka", "video": "https://eudist.animemusicquiz.com/agsvoz.webm", "mp3": "https://eudist.animemusicquiz.com/i3vp9q.mp3"},
-    {"id": 4, "anime": "Akuyaku Reijou Tensei Ojisan - Ending 1", "name": "\"Matsuken Samba II\" by Kazuhiko Inoue, M・A・O", "video": "https://eudist.animemusicquiz.com/e8q47f.webm", "mp3": "https://eudist.animemusicquiz.com/dje0hr.mp3"},
-    {"id": 5, "anime": "Amagami-san Chi no Enmusubi - Ending 2", "name": "\"Kamisama no Iu Toori!\" by ≠ME", "video": "https://eudist.animemusicquiz.com/p4etz1.webm", "mp3": "https://eudist.animemusicquiz.com/wz8z3p.mp3"},
-    {"id": 6, "anime": "Ameku Takao no Suiri Karte - Ending 1", "name": "\"will be fine\" by Gospellers feat. Anly", "video": "https://eudist.animemusicquiz.com/0spf2m.webm", "mp3": "https://eudist.animemusicquiz.com/iagxs7.mp3"},
-    {"id": 7, "anime": "Ao no Exorcist: Yosuga-hen - Ending 1", "name": "\"Overlap\" by Shiyui", "video": "https://eudist.animemusicquiz.com/3z1d8i.webm", "mp3": "https://eudist.animemusicquiz.com/162vdv.mp3"},
-    {"id": 8, "anime": "Ao no Hako - Ending 2", "name": "\"Contrast\" by TOMOO", "video": "https://eudist.animemusicquiz.com/qmcqk3.webm", "mp3": "https://eudist.animemusicquiz.com/l2sgqo.mp3"},
-    {"id": 9, "anime": "Ao no Miburo - Ending 2", "name": "\"Fragment\" by osage", "video": "https://eudist.animemusicquiz.com/8b8iva.webm", "mp3": "https://eudist.animemusicquiz.com/0yt4lm.mp3"},
-    {"id": 10, "anime": "Arafou Otoko no Isekai Tsuuhan - Ending 1", "name": "\"Aicraft\" by Kiminone", "video": "https://eudist.animemusicquiz.com/ohlrpc.webm", "mp3": "https://eudist.animemusicquiz.com/74jbjn.mp3"},
+    {
+        "id": 1,
+        "anime": "Appleseed - ED1",
+        "name": "Crystal Celebration by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/avu2st.webm",
+        "mp3": "https://eudist.animemusicquiz.com/3gmj09.mp3"
+    },
+    {
+        "id": 2,
+        "anime": "Akai Koudan Zillion - OP1",
+        "name": "Pure Stone by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/h9wu84.webm",
+        "mp3": "https://eudist.animemusicquiz.com/k84zcc.mp3"
+    },
+    {
+        "id": 3,
+        "anime": "Akai Koudan Zillion - ED1",
+        "name": "Push! by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/t2ah55.webm",
+        "mp3": "https://eudist.animemusicquiz.com/l4mjfo.mp3"
+    },
+    {
+        "id": 4,
+        "anime": "Akai Koudan Zillion - ED2",
+        "name": "Rock Candy by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/53fi5u.webm",
+        "mp3": "https://eudist.animemusicquiz.com/7bubho.mp3"
+    },
+    {
+        "id": 5,
+        "anime": "Akai Koudan Zillion - IN",
+        "name": "No More Normal by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/7mgg47.webm",
+        "mp3": "https://eudist.animemusicquiz.com/87r7ss.mp3"
+    },
+    {
+        "id": 6,
+        "anime": "Tenkuu Senki Shurato - IN",
+        "name": "Yasashisa no Mukougawa by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/7on6pe.webm",
+        "mp3": "https://eudist.animemusicquiz.com/orn8pc.mp3"
+    },
+    {
+        "id": 7,
+        "anime": "Akai Koudan Zillion: Utahime Yakyoku - OP1",
+        "name": "Nageki no KIDS by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/n6dkqs.webm",
+        "mp3": "https://eudist.animemusicquiz.com/5ay21y.mp3"
+    },
+    {
+        "id": 8,
+        "anime": "Akai Koudan Zillion: Utahime Yakyoku - ED1",
+        "name": "Sayonara no Machikado by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/95r6nj.webm",
+        "mp3": "https://eudist.animemusicquiz.com/6mtfmk.mp3"
+    },
+    {
+        "id": 9,
+        "anime": "Kidou Senshi SD Gundam Mk-II - ED1",
+        "name": "Planet Zone by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/3vqls8.webm",
+        "mp3": "https://eudist.animemusicquiz.com/xl07nj.mp3"
+    },
+    {
+        "id": 10,
+        "anime": "B.B.: Burning Blood - OP1",
+        "name": "BURNING BLOOD by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/4h8yta.webm",
+        "mp3": "https://eudist.animemusicquiz.com/mfxuda.mp3"
+    },
+    {
+        "id": 11,
+        "anime": "B.B.: Burning Blood - ED1",
+        "name": "BELIEVE IN by Risa Youki",
+        "video": "https://eudist.animemusicquiz.com/k86o04.webm",
+        "mp3": "https://eudist.animemusicquiz.com/si39gu.mp3"
+    }
 ] satisfies Song[];
